479f393ce31140d9c3f5f3f8edfb908c8e81651f
Robert Hsu <roberthsu2003@gmail.com>
2020-07-14 11:17:00 +0800

    modify

--- 上課內容/TQC/PYD202.py
+++ 上課內容/TQC/PYD202.py
@@ -33,10 +33,10 @@
 
 a = int(input())
 if (a%3 == 0) and (a%5 == 0):
-    print("%d is a mutiple of 3 and 5." % a)
+    print("%d is a multiple of 3 and 5." % a)
 elif a%3 == 0:
-    print("%d is a mutiple of 3 ." % a)
+    print("%d is a multiple of 3." % a)
 elif a%5 == 0:
-    print("%d is a mutiple of 5." % a)
+    print("%d is a multiple of 5." % a)
 else:
-    print("%d is not a mutiple of 3 and 5." % a)
+    print("%d is not a multiple of 3 or 5." % a)
