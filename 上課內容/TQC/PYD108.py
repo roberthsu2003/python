f4b20cd774b46f23191b874da14666eadda82040
Robert Hsu <roberthsu2003@gmail.com>
2020-07-14 10:30:25 +0800

    modify

--- 上課內容/TQC/PYD108.py
+++ 上課內容/TQC/PYD108.py
@@ -56,6 +56,7 @@ x2 = eval(input())
 y2 = eval(input())
 
 dist = ((x2- x1) ** 2 + (y2-y1) ** 2) ** (0.5)
+
 print("( " , x1 , " , " , y1 , " )")
 print("( " , x2 , " , " , y2 , " )")
 print("Distance = %.4f" % dist)
