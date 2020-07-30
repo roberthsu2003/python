6c20e1c6af659add7ed3a4ef33d116c0e2b43ce3
Robert Hsu <roberthsu2003@gmail.com>
2020-07-30 15:33:32 +0800

    modify

--- 北認下午135上課內容/sample11.py
+++ 北認下午135上課內容/sample11.py
@@ -11,11 +11,24 @@ if __name__ == '__main__':
     window = Tk()
     window.title("BMI應用程式")
     window.geometry("300x200")
+
     mainFrame = Frame(window)
-    
+
     subFrame = Frame(mainFrame, relief=GROOVE, borderwidth=2)
-    titleLabel = Label(mainFrame,text="BMI Control").place(relx=0.05, rely=0.025, anchor=NW)
-    button = Button(subFrame, text="BMI計算").pack(expand=YES, fill=BOTH, padx=40, pady=25)
+
+    height_frame = Frame(subFrame)
+    height_label = Label(height_frame,text='身高(cm)')
+    height_label.pack(side=LEFT)
+    height_entry = Entry(height_frame)
+    height_entry.pack(side=LEFT)
+    height_frame.pack(side=TOP)
+
+    weight_frame = Frame(subFrame)
+    weight_label = Label(weight_frame, text='體重(kg)')
+    weight_label.pack(side=LEFT)
+    weight_entry = Entry(weight_frame)
+    weight_entry.pack(side=LEFT)
+    weight_frame.pack(side=TOP)
     subFrame.pack(expand=YES, fill=BOTH, padx=5, pady=20)
     
     mainFrame.pack(expand=YES, fill=BOTH)
