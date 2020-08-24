43e13563286f6f5dfb5c80b86beef9f69c17a822
Robert Hsu <roberthsu2003@gmail.com>
2020-08-24 14:41:40 +0800

    modify

--- 北認下午12345上課內容/guess.py
+++ 北認下午12345上課內容/guess.py
@@ -1,4 +1,24 @@
 import random
 
-target = random.randint(1,100)
-print(target)
+min = 1
+max = 100
+count = 0
+target = random.randint(min,max)
+print("====================猜數字遊戲===================\n")
+while(True):
+    count += 1
+    keyin = int(input("猜數字範圍%d-%d:" % (min, max)))
+    if(keyin >= min and keyin <= max):
+        if(keyin == target):
+            print("賓果!猜對了, 答案是:%d" % target)
+            print("您猜了%d次" % count)
+            break
+        elif keyin > target:
+            max = keyin
+            print("再小一點")
+        elif keyin < target:
+            min = keyin
+            print("再大一點")
+        print("您猜了%d次\n" % count)
+    else:
+        print("請輸入提示範圍內的數字")
