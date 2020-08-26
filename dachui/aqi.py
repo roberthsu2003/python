a08de3dd3ca3739ec987443ef08c731b6e62c6e0
Robert Hsu <roberthsu2003@gmail.com>
2020-08-26 16:41:04 +0800

    modify

--- dachui/aqi.py
+++ dachui/aqi.py
@@ -1,5 +1,6 @@
 import urllib3
 import certifi
+import csv
 
 csv_url = "https://opendata.epa.gov.tw/api/v1/AQI?%24skip=0&%24top=1000&%24format=csv"
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
@@ -7,3 +8,14 @@ response=http.request('GET', csv_url)
 file=open('空氣品質指標.csv', 'wb')
 file.write(response.data)
 file.close()
+
+file=open('空氣品質指標.csv','r',encoding='UTF-8')
+rows = csv.reader(file)
+
+rowsList = list(rows)
+for rows in rowsList:
+    for row in rows:
+        print(row)
+
+
+
