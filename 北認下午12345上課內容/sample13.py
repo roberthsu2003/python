7dd8ca66874025c46fa53be12b5c820256c35664
Robert Hsu <roberthsu2003@gmail.com>
2020-08-28 17:13:51 +0800

    message

--- 北認下午12345上課內容/sample13.py
+++ 北認下午12345上課內容/sample13.py
@@ -2,12 +2,15 @@ import csv
 
 with open('空氣品質指標.csv','r',encoding='UTF-8') as file:
     rows=csv.reader(file)
-       # for row in rows:
-        #print(row)
-    airList = list(rows)
+    airList = []
+    for row in rows:
+        cityAir = []
+        cityAir.append(row[0])
+        cityAir.append(row[1])
+        cityAir.append(row[2])
+        cityAir.append(row[17])
+        airList.append(cityAir)
     airList.pop(0)
-    print(airList)
-
-
-
+    for air in airList:
+        print(air)
 print("程式結束")
