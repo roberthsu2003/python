4446d0105f49043f87c5e6a64aedc08e8c03de9c
Robert Hsu <roberthsu2003@gmail.com>
2020-11-17 13:58:31 +0800

    modify

--- trainProject/sample2.py
+++ trainProject/sample2.py
@@ -1,19 +1,25 @@
 from tkinter import *
+import time
+import threading
 
 def main():
     w = Tk()
     w.title("我的第一個window")
     w.geometry('500x300')
     Button(w, text="確定1",command=buttonCallBack).pack(ipadx=25,ipady=10,side=TOP,expand=YES)
-    Button(w, text="確定2", command=buttonCallBack).pack(side=TOP,ipadx=25,ipady=10,expand=YES)
-    Button(w, text="確定3", command=buttonCallBack).pack(side=TOP,ipadx=25,ipady=10,expand=YES)
-
-
+    Button(w, text="確定2").pack(side=TOP,ipadx=25,ipady=10,expand=YES)
+    Button(w, text="確定3").pack(side=TOP,ipadx=25,ipady=10,expand=YES)
     w.mainloop()
 
 
 def buttonCallBack():
-    print("Hello! Python!")
+    t1 = threading.Thread(target=doJob);
+    t1.start()
+
+def doJob():
+    while True:
+        print("Hello! Python!")
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
