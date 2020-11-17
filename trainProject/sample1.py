5bacbbe8a82f02582367545cd6af53a86d008e00
Robert Hsu <roberthsu2003@gmail.com>
2020-11-17 11:13:49 +0800

    modify

--- trainProject/sample1.py
+++ trainProject/sample1.py
@@ -1,13 +1,16 @@
-from tkinter import Tk,Button,Label
+from tkinter import *
 
 def main():
     w = Tk()
     w.title("我的第一個window")
     w.geometry('300x300')
-    b = Button(w, text="OK")
+    b = Button(w, text="確定",command=buttonCallBack)
     b.pack()
     w.mainloop()
-    pass
+
+
+def buttonCallBack():
+    print("Hello! Python!")
 
 if __name__ == "__main__":
     main()
