39cb4551ca7da41fcce8727f1dc93fc3b936ba97
Robert Hsu <roberthsu2003@gmail.com>
2020-11-17 14:10:21 +0800

    modify

--- trainProject/sample2.py
+++ trainProject/sample2.py
@@ -3,16 +3,17 @@ import time
 import threading
 
 def main():
+    repeadingSomeThing()
     w = Tk()
     w.title("我的第一個window")
     w.geometry('500x300')
-    Button(w, text="確定1",command=buttonCallBack).pack(ipadx=25,ipady=10,side=TOP,expand=YES)
+    Button(w, text="確定1").pack(ipadx=25,ipady=10,side=TOP,expand=YES)
     Button(w, text="確定2").pack(side=TOP,ipadx=25,ipady=10,expand=YES)
     Button(w, text="確定3").pack(side=TOP,ipadx=25,ipady=10,expand=YES)
     w.mainloop()
 
 
-def buttonCallBack():
+def repeadingSomeThing():
     t1 = threading.Thread(target=doJob);
     t1.start()
 
