a73c492122ba4f9edaa38d5451d85badca6a8359
Robert Hsu <roberthsu2003@gmail.com>
2020-11-18 11:17:43 +0800

    modify

--- trainProject/sample5.py
+++ trainProject/sample5.py
@@ -1,13 +1,17 @@
 from tkinter import *
 
-def main():
-
-    w = Tk()
-    w.title("我的第一個window")
-    w.geometry('500x300')
-    Button(w, text="確定1").pack(ipadx=25,ipady=10,side=LEFT,expand=YES)
-    Button(w, text="確定2").pack(ipadx=25,ipady=10,side=LEFT,expand=YES)
-    Button(w, text="確定3").pack(ipadx=25,ipady=10,side=LEFT,expand=YES)
+def main(w):
+    gui(w)
     w.mainloop()
+
+def gui(w):
+    w.title("收集資料")
+    # w.geometry('500x300')
+    Button(w, text="確定1").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
+    Button(w, text="確定2").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
+    Button(w, text="確定3").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
+
+
 if __name__ == "__main__":
-    main()
+    window = Tk()
+    main(window)
