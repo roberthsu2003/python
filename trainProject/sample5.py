388419d7ad3089a6ca19c409a0adfe92cfdc8c97
Robert Hsu <roberthsu2003@gmail.com>
2020-11-18 11:39:46 +0800

    modify

--- trainProject/sample5.py
+++ trainProject/sample5.py
@@ -7,14 +7,14 @@ def main(w):
 def gui(w):
     w.title("收集資料")
     # w.geometry('500x300')
-    topFrame = Frame(w)
+    topFrame = Frame(w,bd=1,relief=GROOVE)
     Button(topFrame, text="確定1").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
     Button(topFrame, text="確定2").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
     Button(topFrame, text="確定3").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
-    topFrame.pack(ipady=20,ipadx=20)
+    topFrame.pack(ipady=20,ipadx=20,pady=20,padx=20)
 
-    bottomFrame = Frame(w)
-    bottomFrame.pack(ipady=20, ipadx=20)
+    bottomFrame = Frame(w,bd=1,relief=GROOVE)
+    bottomFrame.pack(ipady=20,ipadx=20,pady=20,padx=20,expand=YES,fill=X)
 
 
 if __name__ == "__main__":
