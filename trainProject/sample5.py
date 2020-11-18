b10e7fe6be6fb7f8c86078f4d3a86bc3049be830
Robert Hsu <roberthsu2003@gmail.com>
2020-11-18 11:55:52 +0800

    modify

--- trainProject/sample5.py
+++ trainProject/sample5.py
@@ -8,9 +8,10 @@ def gui(w):
     w.title("收集資料")
     # w.geometry('500x300')
     topFrame = Frame(w,bd=1,relief=GROOVE)
-    Button(topFrame, text="確定1").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
-    Button(topFrame, text="確定2").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
-    Button(topFrame, text="確定3").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
+    topFrame.config(bg="#dddddd")
+    Button(topFrame, text="確定1",font=('Verdans',13,'bold')).pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
+    Button(topFrame, text="確定2",font=('Verdans',13,'bold')).pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
+    Button(topFrame, text="確定3",font=('Verdans',13,'bold')).pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
     topFrame.pack(ipady=20,ipadx=20,pady=20,padx=20)
 
     bottomFrame = Frame(w,bd=1,relief=GROOVE)
