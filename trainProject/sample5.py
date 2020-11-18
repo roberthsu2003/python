e3da42213edc7c149a1c4aecdce46240a5a341c7
Robert Hsu <roberthsu2003@gmail.com>
2020-11-18 14:24:52 +0800

    modify

--- trainProject/sample5.py
+++ trainProject/sample5.py
@@ -11,7 +11,7 @@ def gui(w):
     # w.geometry('500x300')
     topFrame = Frame(w,bd=1,relief=GROOVE)
     topFrame.config(bg="#dddddd")
-    Button(topFrame, text="空氣品質指標",font=('Verdans',13,'bold'),command="aqi").pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
+    Button(topFrame, text="空氣品質指標",font=('Verdans',13,'bold'),command=aqi).pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
     Button(topFrame, text="確定2",font=('Verdans',13,'bold')).pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
     Button(topFrame, text="確定3",font=('Verdans',13,'bold')).pack(ipadx=25, ipady=10, side=LEFT, expand=YES)
     topFrame.pack(ipady=20,ipadx=20,pady=20,padx=20)
@@ -20,7 +20,21 @@ def gui(w):
 
 def aqi():
     # 下載AQI
+    print("下載資料")
     CSV_URL = "http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=csv"
+    http = urllib3.PoolManager()
+    response = http.request('GET', CSV_URL)
+    if response.status == 200:
+        print("下載成功")
+        #儲存檔案
+        file = open("空氣品質指標.csv","wb")
+        file.write(response.data)
+        file.close()
+        print("存檔成功")
+    else:
+        print("下載失敗")
+        return
+    print(response.data)
 
 
 
