bd12101c1c13ae8f3bbc6b08cf2b04fcd9301307
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-27 15:00:01 +0800

    modify

--- 上課內容/12_06_星期日/lesson8-2.py
+++ 上課內容/12_06_星期日/lesson8-2.py
@@ -4,10 +4,12 @@
 '''
 
 def outputGloble():
-    print(a)
-    print(b)
+    a = 20
+    print("func內的a",a)
+    print("func外的b",b)
 
 if __name__ == "__main__":
     a = 5
     b = 10
     outputGloble()
+    print("func外的a", a)
