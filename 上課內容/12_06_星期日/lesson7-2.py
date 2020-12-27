cf7fa750c35af3baa0936f5eef78ec4d4d29a179
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-27 12:16:14 +0800

    modify

--- 上課內容/12_06_星期日/lesson7-2.py
+++ 上課內容/12_06_星期日/lesson7-2.py
@@ -2,22 +2,25 @@
 
 from random import randint
 
+#建立一組亂數樂透
+def randomLot():
+    lot = set()
+    while (len(lot) <= 7):
+        rValue = randint(1, 49)
+        lot.add(rValue)
+    print('本期大樂透電腦選號號碼如下:')
+
+    lotList = list(lot)
+    specialNum = lotList.pop()
+    for item in lotList:
+        print(item, end=' ')
+
+    print('特別號:%d\n' % specialNum)
 
 if __name__ == "__main__":
     count = int(input("請輸入需要的組數:"))
     for _ in range(count):
-        lot = set()
-        while(len(lot) <= 7):
-            rValue = randint(1, 49)
-            lot.add(rValue)
-        print('本期大樂透電腦選號號碼如下:')
-
-        lotList = list(lot)
-        specialNum = lotList.pop()
-        for item in lotList:
-            print(item, end=' ')
-
-        print('特別號:%d\n' % specialNum )
+        randomLot()
 
 
 
