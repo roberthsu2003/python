e7f2041e1ab0d33e37175ff9216be2774759e463
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-27 11:57:07 +0800

    modify

--- 上課內容/12_06_星期日/lesson7-2.py
+++ 上課內容/12_06_星期日/lesson7-2.py
@@ -9,4 +9,12 @@ if __name__ == "__main__":
         rValue = randint(1, 49)
         lot.add(rValue)
     print('本期大樂透電腦選號號碼如下:')
-    print(lot)
+
+    lotList = list(lot)
+    specialNum = lotList.pop()
+    for item in lotList:
+        print(item, end=' ')
+
+    print('特別號:%d' % specialNum )
+
+
