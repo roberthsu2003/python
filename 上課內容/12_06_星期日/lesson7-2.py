1066f1775caace5d7c7205c26a29df0835d5cb0f
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-27 12:06:05 +0800

    modify

--- 上課內容/12_06_星期日/lesson7-2.py
+++ 上課內容/12_06_星期日/lesson7-2.py
@@ -4,17 +4,20 @@ from random import randint
 
 
 if __name__ == "__main__":
-    lot = set()
-    while(len(lot) <= 7):
-        rValue = randint(1, 49)
-        lot.add(rValue)
-    print('本期大樂透電腦選號號碼如下:')
+    count = int(input("請輸入需要的組數:"))
+    for _ in range(count):
+        lot = set()
+        while(len(lot) <= 7):
+            rValue = randint(1, 49)
+            lot.add(rValue)
+        print('本期大樂透電腦選號號碼如下:')
 
-    lotList = list(lot)
-    specialNum = lotList.pop()
-    for item in lotList:
-        print(item, end=' ')
+        lotList = list(lot)
+        specialNum = lotList.pop()
+        for item in lotList:
+            print(item, end=' ')
 
-    print('特別號:%d' % specialNum )
+        print('特別號:%d\n' % specialNum )
+
 
 
