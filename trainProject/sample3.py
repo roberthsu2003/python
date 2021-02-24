6dc301bfbdfe351d0e3d68c1acd06ce588968ad0
roberthsu2003 <roberthsu2003@gmail.com>
2021-02-24 21:35:48 +0800

    modify

--- trainProject/sample3.py
+++
@@ -1,8 +0,0 @@
-import lineTool
-token = ""
-msg = "Python 語言整合通訊軟體,恭喜您"
-response=lineTool.lineNotify(token,msg)
-if response == 200:
-    print("傳送成功")
-else:
-    print("傳送失敗")
--- 上課內容/12_06_星期日/sample.py
+++
@@ -1,5 +0,0 @@
-a = 100
-print("a=", a)
-b = 50
-print("b=", b)
-print("a+b=", a + b)
