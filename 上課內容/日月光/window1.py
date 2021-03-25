544c24f8119438ca065691815440fb68c15edebe
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-25 17:20:15 +0800

    modify

--- 上課內容/日月光/window1.py
+++ 上課內容/日月光/window1.py
@@ -4,6 +4,7 @@ from tkinter import Tk,Label,BOTH
 if __name__ == '__main__':
     window = Tk()
     window.title("這是我的第一個視窗軟體")
+    window.geometry('600x300+200+100')
     lable = Label(window, text="Hello! Tkinter")
-    lable.pack(padx=100,pady=50)
+    lable.pack(padx=100,pady=50,expand=True)
     window.mainloop()
