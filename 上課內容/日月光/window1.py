a7bd799e4c352bfabe18ae575ff763f74107ad53
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-25 17:12:42 +0800

    modify

--- 上課內容/日月光/window1.py
+++ 上課內容/日月光/window1.py
@@ -1,6 +1,9 @@
-from tkinter import Tk
+from tkinter import Tk,Label,BOTH
+
 
 if __name__ == '__main__':
     window = Tk()
     window.title("這是我的第一個視窗軟體")
+    lable = Label(window, text="Hello! Tkinter")
+    lable.pack(padx=100,pady=50)
     window.mainloop()
