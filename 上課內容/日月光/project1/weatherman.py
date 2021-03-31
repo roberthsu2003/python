2e26e4647d1f54eae0c93d584f85253707bcae90
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-31 16:43:06 +0800

    modify

--- 上課內容/日月光/project1/weatherman.py
+++ 上課內容/日月光/project1/weatherman.py
@@ -1,8 +1,13 @@
 #主程式
-import report
+#import report as rp
+from tools.report import get_description
+from tools.report import a
+from tools.report import b
+from tools.report import c
+
 if __name__ == "__main__":
-    description = report.get_description()
+    description = get_description()
     print("今天的天氣是"+description)
-    print("module report內的a是",report.a)
-    print("module report內的b是", report.b)
-    print("module report內的c是", report.c)
+    print("module report內的a是",a)
+    print("module report內的b是", b)
+    print("module report內的c是", c)
