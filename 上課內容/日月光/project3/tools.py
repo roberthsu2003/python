1a93ddee404ca7299abefdb09c7e1d12cfdb1c72
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-01 17:16:25 +0800

    modify

--- 上課內容/日月光/project3/tools.py
+++ 上課內容/日月光/project3/tools.py
@@ -1,9 +1,49 @@
 class Student:
     def __init__(self,name,chinese=0,english=0,math=0):
         self.name = name
-        self.chinese = chinese
-        self.english = english
-        self.math = math
+        self.__chinese = chinese
+        self.__english = english
+        self.__math = math
+
+    @property
+    def math(self):
+        return self.__math
+
+    @math.setter
+    def math(self, value):
+        if value > 100:
+            self.__math = 100
+        elif value < 0:
+            self.__math = 0
+        else:
+            self.__math = value
+
+    @property
+    def english(self):
+        return self.__english
+
+    @english.setter
+    def english(self, value):
+        if value > 100:
+            self.__english = 100
+        elif value < 0:
+            self.__english = 0
+        else:
+            self.__english = value
+
+    @property
+    def chinese(self):
+        return self.__chinese
+
+    @chinese.setter
+    def chinese(self,value):
+        if value > 100:
+            self.__chinese = 100
+        elif value < 0:
+            self.__chinese = 0
+        else:
+            self.__chinese = value
+
     @property
     def sum(self):
         return self.chinese+self.english+self.math
