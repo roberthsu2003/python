89d50800315b011c59d8defe50f47322c0fd7990
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-07 16:53:30 +0800

    modify

--- 上課內容/日月光/project4/sample1.py
+++ 上課內容/日月光/project4/sample1.py
@@ -10,11 +10,17 @@ class Window(tk.Tk):
         label = tk.Label(self, text="Choose One", padx=100, pady=30, font=('Times', 24, 'bold italic'))
         label.pack(expand=True)
 
-        hello_button = tk.Button(self, text="Say Hello",padx=20,pady=10,font=('Times', 20))
+        hello_button = tk.Button(self, text="Say Hello",padx=20,pady=10,font=('Times', 20), command=self.say_hello)
         hello_button.pack(side=tk.LEFT, padx=(20,0), pady=(0,20))
 
-        goodbye_button = tk.Button(self, text="Say Goodbye", padx=20, pady=10, font=('Times', 20))
+        goodbye_button = tk.Button(self, text="Say Goodbye", padx=20, pady=10, font=('Times', 20), command=self.say_goodbye)
         goodbye_button.pack(side=tk.RIGHT, padx=(20, 20), pady=(0, 20))
+
+    def say_hello(self):
+        print("say_hello")
+
+    def say_goodbye(self):
+        print("say_goodbye")
 
 if __name__ == "__main__":
     window = Window()
