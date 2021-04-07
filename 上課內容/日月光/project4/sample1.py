bf9e677a5870555848d861c55b16313fe5437c09
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-07 16:10:40 +0800

    modify

--- 上課內容/日月光/project4/sample1.py
+++ 上課內容/日月光/project4/sample1.py
@@ -5,9 +5,10 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Hello Tkinter")
+        #self.geometry("400x200")
 
-        label = tk.Label(self, text="Hello First Window")
-        label.pack(fill=tk.BOTH, expand=tk.YES, padx=100, pady=50)
+        label = tk.Label(self, text="Hello First Window", padx=100, pady=50, font=('Times', 24, 'bold italic'))
+        label.pack(expand=True)
 
 if __name__ == "__main__":
     window = Window()
