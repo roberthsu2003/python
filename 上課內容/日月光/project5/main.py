e43e3bda04a90d84999624cb552c5d8f5bfd8ae2
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-15 14:38:18 +0800

    modify

---
+++ 上課內容/日月光/project5/main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Line Bot")
+        font = ('arial',20)
+        lineButton = tk.Button(self,text='send Line Message',padx=30, pady=30,command=self.sendLineMessage,font=font)
+        lineButton.pack(padx=20,pady=20)
+
+    def sendLineMessage(self):
+        print("sendLineMessage")
+
+
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()
