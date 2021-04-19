c57280bc63a5beb1939ecdc504801b78126c3b5b
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-19 11:21:36 +0800

    modify

--- 上課內容/日月光/lesson15.py
+++
@@ -1,8 +0,0 @@
-def abc():
-    global a
-    a = 10
-
-
-a = 5
-abc()
-print(a)
--- 上課內容/日月光/project5/linebot/__init__.py
+++
@@ -1,9 +0,0 @@
-import requests
-token = "KDkQLircjHImzJKTPcXKChQo0LlC5W1s3UVl9V3R9K6"
-def sendLineNotify( msg, picURI):
-    url = "https://notify-api.line.me/api/notify"
-    headers = {"Authorization":"Bearer " + token}
-    payload= {"message":msg}
-    files = {"imageFile":open(picURI,'rb')}
-    response=requests.post(url, headers = headers, params = payload,files = files)
-    return response.status_code
