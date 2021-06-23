6012ea7c26510a3f698d650249bab1a6be83e8a2
Robert Hsu <roberthsu2003@gmail.com>
2021-06-23 12:30:44 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,3 +1,3 @@
-a = int(input("請輸入除數:"))
-b = int(input("請輸入被除數:"))
-print("除數:",a,"被除數:",b,"商=",a // b,"餘數=", a % b)
+radius = int(input("請輸入半徑:"))
+area = radius * radius * 3.14
+print("圓面積:",area)
