7775ff61090c86a4564af1329d4765b50c3adf7a
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-06 11:29:34 +0800

    modify

---
+++ 職能發展學院/lesson2_2.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3.x
+
+import tkinter as tk
+
+if __name__ == "__main__":
+    print("這個是主執行檔")
+    window = tk.Tk()
+    window.title("這是我的第一個視窗")
+    window.geometry("300x300")
+    window.mainloop()
