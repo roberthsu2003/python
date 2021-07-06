09bd2766a5d4af4cc496b7f9ac345aa13ca42477
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-06 13:57:28 +0800

    modify

--- 職能發展學院/lesson2_2.py
+++ 職能發展學院/lesson2_2.py
@@ -2,9 +2,14 @@
 
 import tkinter as tk
 
+def createInterface(w):
+    label = tk.Label(w, text="Hello! tkinter")
+    label.pack(padx=100,pady=50)
+
 if __name__ == "__main__":
     print("這個是主執行檔")
     window = tk.Tk()
     window.title("這是我的第一個視窗")
-    window.geometry("300x300")
+    #window.geometry("300x300")
+    createInterface(window)
     window.mainloop()
