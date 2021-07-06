bc2dfe4908af240559671d2edec36bb54cbf43f1
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-06 15:33:38 +0800

    modify

--- 職能發展學院/lesson2_4.py
+++ 職能發展學院/lesson2_4.py
@@ -7,10 +7,18 @@ class Window(tk.Tk):
         self.title("這是物件導向的寫法")
         lable = tk.Label(self,text="請選擇其中一個按鈕")
         lable.pack(padx=100,pady=50)
-        hello_button = tk.Button(self, text="Say Hello",padx=10, pady=10)
-        hello_button.pack(side=tk.LEFT,padx=(20,0),pady=(0,20))
-        goodbye_button = tk.Button(self,text="再見",padx=10, pady=10)
-        goodbye_button.pack(side=tk.RIGHT,padx=(0,20),pady=(0,20))
+        helloButton = tk.Button(self, text="Say Hello",padx=10, pady=10,command=self.say_hello)
+        helloButton.pack(side=tk.LEFT,padx=(20,0),pady=(0,20))
+        goodbyeButton = tk.Button(self,text="再見",padx=10, pady=10,command=self.say_goodbye)
+        goodbyeButton.pack(side=tk.RIGHT,padx=(0,20),pady=(0,20))
+
+    def say_hello(self):
+        print("sayHello")
+
+    def say_goodbye(self):
+        print("sayGoodbye")
+
+
 
 if __name__ == "__main__":
     window = Window()
