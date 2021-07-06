10cdc4445c18012a413f2d3854b11ace56a93d4c
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-06 16:47:17 +0800

    modify

--- 職能發展學院/lesson2_5.py
+++ 職能發展學院/lesson2_5.py
@@ -1,30 +1,4 @@
-import tkinter as tk
-
-class Window(tk.Tk):
-    #override init
-    def __init__(self):
-        super().__init__()
-        self.title("這是物件導向的寫法")
-        self.message = tk.Label(self,text="訊息:")
-        self.message.pack(padx=100,pady=50)
-        leftButton = tk.Button(self, text="LEFT Button",padx=10, pady=10,command=self.leftButtonClick)
-        leftButton.pack(side=tk.LEFT,padx=(20,0),pady=(0,20))
-        centerButton = tk.Button(self,text="Center Button",padx=10, pady=10,command=self.centerButtonClick)
-        centerButton.pack(side=tk.LEFT,pady=(0,20))
-        rightButton = tk.Button(self,text="right Button",padx=10, pady=10,command=self.rightButtonClick)
-        rightButton.pack(side=tk.LEFT,padx=(0,20),pady=(0,20))
-
-    def leftButtonClick(self):
-        self.message.configure(text="訊息:左按鈕被按了")
-
-    def centerButtonClick(self):
-        self.message.configure(text="訊息:中間按鈕被按了")
-
-
-    def rightButtonClick(self):
-        self.message.configure(text="訊息:右按鈕被按了")
-
-
+from windowUI import Window
 
 if __name__ == "__main__":
     window = Window()
