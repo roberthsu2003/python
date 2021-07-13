acf764cd40477165d6f533a725cfd318d489c835
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-13 11:43:16 +0800

    modify

--- 職能發展學院/lesson3_2.py
+++ 職能發展學院/lesson3_2.py
@@ -4,12 +4,21 @@ import tkinter as tk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        #取得網路上的資料
         res = requests.get('https://flask-robert.herokuapp.com/youbike')
         jsonObj = res.json()
         areas = jsonObj['areas']
+
+        #介面
+        topFrame = tk.Frame(self)
         for area in areas:
-            print(area)
+            tk.Button(topFrame,text=area).pack(side=tk.LEFT)
+        topFrame.pack(padx=100, pady=(30,60))
+
+
+
 
 if __name__ == "__main__":
     window = Window()
+    window.title("台北市行政區")
     window.mainloop()
