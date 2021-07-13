6be7bf9f695d861afef54886504b85d3f9912efd
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-13 11:14:36 +0800

    modify

--- 職能發展學院/lesson3_2.py
+++ 職能發展學院/lesson3_2.py
@@ -1,8 +1,15 @@
 import requests
+import tkinter as tk
 
-res = requests.get('https://flask-robert.herokuapp.com/youbike')
-jsonObj = res.json()
-print(type(jsonObj))
-areas=jsonObj['areas']
-for area in areas:
-    print(area)
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        res = requests.get('https://flask-robert.herokuapp.com/youbike')
+        jsonObj = res.json()
+        areas = jsonObj['areas']
+        for area in areas:
+            print(area)
+
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()
