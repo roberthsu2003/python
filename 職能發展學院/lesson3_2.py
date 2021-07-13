b564bca7b6e61e0775108c157d1007691c7ac5b5
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-13 16:48:33 +0800

    modify

--- 職能發展學院/lesson3_2.py
+++ 職能發展學院/lesson3_2.py
@@ -23,9 +23,11 @@ class Window(tk.Tk):
 
         bottomFrame = tk.Frame(self,bd=2,relief=tk.GROOVE,padx=20,pady=10)
         radioButtonData = ['仁愛林森路口', '捷運善導寺站(1號出口)', '南昌公園', '國家圖書館', '捷運臺大醫院(4號出口)', '信義連雲街口', '捷運西門站(3號出口)', '和平重慶路口', '金山市民路口', '華山文創園區', '臺北市客家文化主題公園', '捷運小南門站(1號出口)', '臺北轉運站', '羅斯福寧波東街口', '河堤國小', '植物園', '捷運古亭站(2號出口)', '臺北市立大學', '信義杭州路口(中華電信總公司)', '中山堂', '螢橋國小', '濟南紹興路口', '牯嶺公園', '自來水園區', '捷運忠孝新生站(2號出口)', '光華商場', '莒光大埔街口', '重慶南海路口', '中正運動中心', '博愛寶慶路口', '中山青島路口', '紀州庵', '南門國中', '聯合醫院和平院區']
-        for data in radioButtonData:
-            radioButton = tk.Radiobutton(bottomFrame,text=data).pack(anchor=tk.W)
+        self.var = tk.IntVar()
+        for index,data in enumerate(radioButtonData):
+            radioButton = tk.Radiobutton(bottomFrame,text=data,value=index,variable=self.var).pack(anchor=tk.W)
         bottomFrame.pack(padx=100, pady=(30,60),expand=True,fill=tk.X)
+        self.var.set(0)
 
     def userClick(self,event):
         selectedArea = event.widget['text']
