fc5735aa26432770c0d54776599e5152ab3c1851
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-13 14:28:24 +0800

    modify

--- 職能發展學院/lesson3_2.py
+++ 職能發展學院/lesson3_2.py
@@ -15,11 +15,14 @@ class Window(tk.Tk):
         topFrame = tk.Frame(self,bd=2,relief=tk.GROOVE,padx=20,pady=10)
         buttonFont = Font(family='Helvetica', size=20)
         for area in areas:
-            tk.Button(topFrame,text=area,font=buttonFont,padx=5,pady=5,command=self.userClick).pack(side=tk.LEFT,padx=5)
+            btn = tk.Button(topFrame,text=area,font=buttonFont,padx=5,pady=5)
+            btn.bind('<Button-1>',self.userClick)
+            btn.pack(side=tk.LEFT,padx=5)
+
         topFrame.pack(padx=100, pady=(30,60))
 
-    def userClick(self):
-        print("user click")
+    def userClick(self,event):
+        print(event.widget['text'])
 
 
 
