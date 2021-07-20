8ca089fbb0ec8d50eb9828891135779af8eeda01
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-20 16:19:51 +0800

    modify

--- 職能發展學院/lesson3_2.py
+++ 職能發展學院/lesson3_2.py
@@ -25,19 +25,16 @@ class Window(tk.Tk):
             btn.pack(side=tk.LEFT, padx=5)
         topFrame.pack(padx=20, pady=30)
 
+
+
         self.fixedWidthFrame = tk.Frame(self,width=500,bg='red')
         self.createdBottomFrame()
         self.fixedWidthFrame.pack(side=tk.LEFT,padx=20)
 
-        #自動執行按鈕按一次的事件
-        self.userClick()
 
-    def userClick(self,event=None):
+    def userClick(self,event):
         self.bottomFrame.destroy()
-        if(event == None):
-            selectedArea = "中山區"
-        else:
-            selectedArea = event.widget['text']
+        selectedArea = event.widget['text']
         urlString = "https://flask-robert.herokuapp.com/youbike/%s" % selectedArea
         res = requests.get(urlString)
         jsonobj = res.json()
@@ -47,14 +44,18 @@ class Window(tk.Tk):
             snaList.append(area["sna"])
         self.createdBottomFrame(data=snaList)
 
+
     def createdBottomFrame(self,data=None):
         self.bottomFrame = tk.Frame(self.fixedWidthFrame, bd=2, relief=tk.GROOVE, padx=20, pady=10)
         if data == None:
-            self.radioButtonData = ['仁愛林森路口', '捷運善導寺站(1號出口)', '南昌公園', '國家圖書館', '捷運臺大醫院(4號出口)', '信義連雲街口', '捷運西門站(3號出口)', '和平重慶路口',
-                               '金山市民路口', '華山文創園區', '臺北市客家文化主題公園', '捷運小南門站(1號出口)', '臺北轉運站', '羅斯福寧波東街口', '河堤國小', '植物園',
-                               '捷運古亭站(2號出口)', '臺北市立大學', '信義杭州路口(中華電信總公司)', '中山堂', '螢橋國小', '濟南紹興路口', '牯嶺公園', '自來水園區',
-                               '捷運忠孝新生站(2號出口)', '光華商場', '莒光大埔街口', '重慶南海路口', '中正運動中心', '博愛寶慶路口', '中山青島路口', '紀州庵', '南門國中',
-                               '聯合醫院和平院區']
+            urlString = "https://flask-robert.herokuapp.com/youbike/南港區"
+            res = requests.get(urlString)
+            jsonobj = res.json()
+            self.areas = jsonobj['data']
+            snaList = []
+            for area in self.areas:
+                snaList.append(area["sna"])
+            self.radioButtonData = snaList
         else:
             self.radioButtonData = data
 
@@ -74,5 +75,4 @@ class Window(tk.Tk):
 
 if __name__ == "__main__":
     window = Window()
-    #window.title("台北市行政區")
     window.mainloop()
