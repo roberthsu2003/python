21cc4d12dcb0ac5756dbfe93bca4e2fb1fb4f0f9
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-20 11:10:06 +0800

    modify

--- 職能發展學院/lesson4_1.py
+++ 職能發展學院/lesson4_1.py
@@ -5,13 +5,11 @@ from tkinter.font import Font
 
 
 class Window(tk.Tk):
-
-
     def __init__(self):
         super().__init__()
         self.title("Lesson4_1")
         # 建立系統基本字型
-        buttonFontStyle = Font(family="Lucida Grande", size=25)
+        buttonFontStyle = Font(size=25)
         Button(self,text='LEFT',width=15,font=buttonFontStyle,pady=5).pack(side=tk.LEFT,padx=(20,10),pady=30)
         Button(self, text='Center',width=15,font=buttonFontStyle,pady=5).pack(side=tk.LEFT,padx=10)
         Button(self, width=15,text='Right',font=buttonFontStyle,pady=5).pack(side=tk.RIGHT,padx=(10,20))
