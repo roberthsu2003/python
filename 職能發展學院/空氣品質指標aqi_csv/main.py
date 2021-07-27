2508930a943eaba3e03b6c409a445a4b4251f3ca
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-27 09:16:33 +0800

    modify

--- 職能發展學院/空氣品質指標aqi_csv/main.py
+++ 職能發展學院/空氣品質指標aqi_csv/main.py
@@ -1,6 +1,14 @@
 import data
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        for county in countyList:
+            print(county.siteName, county.name, county.AQI, county.status, county.publishTime)
 
 if __name__ == "__main__":
     countyList = data.aqiData
-    for county in countyList:
-        print(county.siteName,county.name,county.AQI,county.status,county.publishTime)
+    window = Window()
+    window.mainloop()
+
