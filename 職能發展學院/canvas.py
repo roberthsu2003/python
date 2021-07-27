15d7eddd7aaf93ef2315a4222665190ab92ab44e
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-27 15:14:42 +0800

    modify

--- 職能發展學院/canvas.py
+++ 職能發展學院/canvas.py
@@ -1,7 +1,18 @@
-from tkinter import Tk,Canvas
+from tkinter import Tk,Canvas,Frame,Label
+import tkinter as tk
 window = Tk()
 window.title("Canvas")
 canvas = Canvas(window, width=400, height=400, bg='#cccccc')
 canvas.create_oval(10,10,100,100,fill='white')
+canvas.create_line(105,10,200,100, fill='red')
+canvas.create_rectangle(205,10,300,105, outline='white', fill='#777777')
+
+xy = 10, 105, 100, 200
+canvas.create_arc(xy,start=0, extent=270, fill='gray60')
+
+frm = Frame(canvas,relief=tk.GROOVE,borderwidth=2)
+Label(frm, text="嵌入 Frame/Label").pack()
+canvas.create_window(280, 280, window=frm)
+
 canvas.pack()
 window.mainloop()
