c720d3de9e61cbb748ebb1cd9a4e275e5f15871a
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-27 10:05:55 +0800

    modify

--- 職能發展學院/空氣品質指標aqi_csv/main.py
+++ 職能發展學院/空氣品質指標aqi_csv/main.py
@@ -8,13 +8,19 @@ class Window(tk.Tk):
         for county in countyList:
             print(county.siteName, county.name, county.AQI, county.status, county.publishTime)
         '''
-        titleFrame = tk.Frame(self,bg="#9CEFB5",borderwidth = 3,relief=tk.GROOVE)
-        tk.Label(titleFrame,text="全省空氣品質指標_AQI",font=("Courier", 25, "italic")).pack()
+        titleFrame = tk.Frame(self)
+        borderFrame = tk.Frame(titleFrame,borderwidth = 1, relief=tk.GROOVE,padx=20,pady=20)
+        tk.Label(borderFrame,text="全省空氣品質指標_AQI",font=("Courier", 25, "italic")).pack()
+        publishTimeLabel = tk.Label(borderFrame,text="發佈時間:2021-07-27 10:00:00")
+        publishTimeLabel.pack()
+        tk.Button(borderFrame,text="更新",padx=10,pady=10).pack(pady=(20,0),anchor=tk.E)
+        borderFrame.pack()
         titleFrame.pack(padx=20,pady=20)
 
 
 if __name__ == "__main__":
     countyList = data.aqiData
     window = Window()
+    window.title("AQI指標")
     window.mainloop()
 
