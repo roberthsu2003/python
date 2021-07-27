dd154e3533c52444c86ccf7a211e1d871b2a0a65
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-27 13:40:34 +0800

    modify

--- 職能發展學院/空氣品質指標aqi_csv/main.py
+++ 職能發展學院/空氣品質指標aqi_csv/main.py
@@ -13,28 +13,36 @@ class Window(tk.Tk):
         tk.Label(borderFrame,text="全省空氣品質指標_AQI",font=("Courier", 25, "italic")).pack()
         publishTimeLabel = tk.Label(borderFrame,text="發佈時間:"+countyList[0].publishTime)
         publishTimeLabel.pack()
-        tk.Button(borderFrame,text="更新",padx=10,pady=10).pack(pady=(20,0),anchor=tk.E)
+        tk.Button(borderFrame,text="更新",padx=10,pady=10,command=self.userClickUpdate).pack(pady=(20,0),anchor=tk.E)
         borderFrame.pack()
         titleFrame.pack(padx=20,pady=20)
 
-        #下方的frame
+        #建立下方的frame
+        self.createDisplayFrame()
+
+
+    def createDisplayFrame(self):
+        # 下方的frame
         displayFrame = tk.Frame(self)
-        #設定分欄
+        # 設定分欄
         columnsNum = 5
         rowsNum = len(countyList) // columnsNum + 1
-        for index,county in enumerate(countyList):
+        for index, county in enumerate(countyList):
             subIndex = index % rowsNum
             if subIndex == 0:
-                tableFrame = tk.Frame(displayFrame,bg='#cccccc')
-                tableFrame.pack(side=tk.LEFT,padx=(20,0),expand=True,fill=tk.Y)
-            tk.Label(tableFrame, text=county.name,bg='#cccccc').grid(row=subIndex, column=0);
-            tk.Label(tableFrame, text=county.siteName,bg='#cccccc').grid(row=subIndex, column=1);
-            tk.Label(tableFrame, text=county.AQI,bg='#cccccc').grid(row=subIndex, column=2);
-            statusLabel = tk.Label(tableFrame, text=county.status,bg='#cccccc');
+                tableFrame = tk.Frame(displayFrame, bg='#cccccc')
+                tableFrame.pack(side=tk.LEFT, padx=(20, 0), expand=True, fill=tk.Y)
+            tk.Label(tableFrame, text=county.name, bg='#cccccc').grid(row=subIndex, column=0);
+            tk.Label(tableFrame, text=county.siteName, bg='#cccccc').grid(row=subIndex, column=1);
+            tk.Label(tableFrame, text=county.AQI, bg='#cccccc').grid(row=subIndex, column=2);
+            statusLabel = tk.Label(tableFrame, text=county.status, bg='#cccccc');
             if county.status != '良好':
                 statusLabel['fg'] = 'red'
             statusLabel.grid(row=subIndex, column=3)
         displayFrame.pack()
+
+    def userClickUpdate(self):
+        print("update")
 
 
 
