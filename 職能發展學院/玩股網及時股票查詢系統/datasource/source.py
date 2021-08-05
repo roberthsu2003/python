6507c44dd1f47f96099faf09c3644458c1f25703
roberthsu2003 <roberthsu2003@gmail.com>
2021-08-05 14:27:54 +0800

    modify

--- 職能發展學院/玩股網及時股票查詢系統/datasource/source.py
+++ 職能發展學院/玩股網及時股票查詢系統/datasource/source.py
@@ -1,2 +1,46 @@
-def add(a,b):
-    return a+b
+import requests
+from .stockInfo import StockInfo
+from requests import HTTPError, ConnectionError, Timeout
+
+
+def getData(stock):
+    stockInfo = StockInfo()
+    stockInfo.id = stock
+    url = "https://www.wantgoo.com/investrue/" + stock + "/daily-candlestick"
+    header = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
+    try:
+        response = requests.get(url, headers=header)
+        response.raise_for_status()
+    except HTTPError as e:
+        print("伺服器錯誤")
+        print(f"錯誤:{e}")
+        stockInfo.error = e
+    except ConnectionError as e:
+        print("連線錯誤")
+        print(f"錯誤:{e}")
+        stockInfo.error = e
+    except Timeout as e:
+        print("伺服器忙錄")
+        print(f"錯誤:{e}")
+        stockInfo.error = e
+    else:
+        try:
+            json = response.json()
+            stockInfo.time = json["time"]
+            stockInfo.open = json["open"]
+            stockInfo.close = json["close"]
+            stockInfo.volume = json["volume"]
+            stockInfo.millionAmount = json["millionAmount"]
+            stockInfo.high = json["high"]
+            stockInfo.low = json["low"]
+        except requests.exceptions.JSONDecodeError as e:
+            print("json解析錯誤")
+            stockInfo.error = "json解析錯誤"
+
+    infoURL = "https://www.wantgoo.com/stock/" + stock + "/company-profile-data"
+    infoResponse = requests.get(infoURL, headers=header)
+    infoJson = infoResponse.json()
+    stockInfo.name = infoJson["name"]
+
+    return stockInfo
--- 職能發展學院/玩股網及時股票查詢系統/main.py
+++ 職能發展學院/玩股網及時股票查詢系統/main.py
@@ -1,4 +1,12 @@
-from datasource.source import add
+from datasource.source import getData
 
 if __name__ == "__main__":
-    print(add(5,11))
+    stockID = input("請輸入股票代號:")
+    data = getData(stockID)
+    if not data.error:
+        # 沒有錯誤
+        print(f"股票代號:{data.id}")
+        print(f"股票名稱:{data.name}")
+        print(f"收盤價:{data.close}")
+    else:
+        print("資類取得錯誤", data.error)
