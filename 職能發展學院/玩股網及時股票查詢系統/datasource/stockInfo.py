74ecd8395d0c1c6366d4f5be24aed68820afc8af
roberthsu2003 <roberthsu2003@gmail.com>
2021-08-05 11:51:19 +0800

    modify

---
+++ 職能發展學院/玩股網及時股票查詢系統/datasource/stockInfo.py
@@ -0,0 +1,12 @@
+class StockInfo():
+    def __init__(self):
+        self.name = None
+        self.id = None
+        self.time = None
+        self.open = None
+        self.close = None
+        self.volume = None
+        self.millionAmount = None
+        self.high = None
+        self.low = None
+        self.error = None
