7d7263edaf2ea8f0d4e3e05fcbf48fe02a6566f8
roberthsu2003 <roberthsu2003@gmail.com>
2021-08-15 10:32:38 +0800

    modify

--- 上課內容/lesson7_function1.py
+++ 上課內容/lesson7_function1.py
@@ -1,2 +1,9 @@
-print(__name__)
-print(type(__name__))
+#!/bin/python3.8
+
+def temperature(value_c):
+    return 1.8 * value_c + 32
+
+
+if __name__ == "__main__":
+    print(f'攝氏10度轉華氏溫度={temperature(10)}')
+    print('-------------------------')
