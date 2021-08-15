d408679a4214d83749e7bab664c8eb340278c867
roberthsu2003 <roberthsu2003@gmail.com>
2021-08-15 10:49:30 +0800

    modify

--- 上課內容/lesson7_function1.py
+++ 上課內容/lesson7_function1.py
@@ -5,5 +5,16 @@ def temperature(value_c):
 
 
 if __name__ == "__main__":
+    runAgain = True
     print(f'攝氏10度轉華氏溫度={temperature(10)}')
     print('-------------------------')
+    while(runAgain):
+        c = int(input('請輸入攝氏溫度:'))
+        result = temperature(c)
+        print(f"華氏溫度={result}")
+        inputWord=input("程式還要繼續嗎?(輸入N...結束)")
+        if inputWord.upper() == 'N':
+            runAgain = False
+        else:
+            runAgain = True
+    print("程式結束")
