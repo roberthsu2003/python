9f06b08a0aed605d71ccfc40b91b7e58e4545856
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-18 20:30:32 +0800

    modify

--- 上課內容13/lesson7/lesson7_3.py
+++ 上課內容13/lesson7/lesson7_3.py
@@ -1,2 +1,6 @@
 import tools
 tools.get_hello("robert")
+
+person = tools.getPerson("jenny")
+print("type",type(person))
+print("name", person.name)
--- 上課內容13/lesson7/tools.py
+++ 上課內容13/lesson7/tools.py
@@ -1,2 +1,10 @@
 def get_hello(name):
     print(f'hello!{name}');
+
+def getPerson(userName):
+    p1 = Person(name=userName)
+    return p1
+
+class Person():
+    def __init__(self,name):
+        self.name=name
