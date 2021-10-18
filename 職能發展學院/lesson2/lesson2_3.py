81171ae634b0169b93c342b77623417724cc05c6
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-18 16:34:44 +0800

    modify

--- 職能發展學院/lesson2/lesson2_3.py
+++ 職能發展學院/lesson2/lesson2_3.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 window=tk.Tk()
 window.title("Hello Tkinter")
-label = tk.Label(window,text="Hello World")
+window.geometry("500x100")
+label = tk.Label(window,text="Hello World",bg='yellow', fg='#263238', font=('Arial', 20))
 label.pack()
 window.mainloop()
