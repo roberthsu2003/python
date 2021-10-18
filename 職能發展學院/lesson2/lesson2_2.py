84f74ed2272d108bb1d3ac8b9926765f8caa43f4
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-18 15:55:50 +0800

    modify

--- 職能發展學院/lesson2/lesson2_2.py
+++ 職能發展學院/lesson2/lesson2_2.py
@@ -1,13 +1,22 @@
 import requests
-youbikeURL = 'https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json'
-res = requests.get(youbikeURL)
-youbikeData = res.json()
-allSiteDic = youbikeData['retVal']
-allStations = list(allSiteDic.values())
-for station in allStations:
-    print(f"站名:{station['sna']}")
-    print(f"時間:{station['mday']}")
-    print(f"總車數:{station['tot']}")
-    print(f"可還數:{station['bemp']}")
-    print(f"可借數:{station['sbi']}")
-    print('-----------------------')
+def getData():
+    youbikeURL = 'https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json'
+    res = requests.get(youbikeURL)
+    youbikeData = res.json()
+    allSiteDic = youbikeData['retVal']
+    allStations = list(allSiteDic.values())
+    return allStations
+
+
+def displayData(data):
+    for station in data:
+        print(f"站名:{station['sna']}")
+        print(f"時間:{station['mday']}")
+        print(f"總車數:{station['tot']}")
+        print(f"可還數:{station['bemp']}")
+        print(f"可借數:{station['sbi']}")
+        print('-----------------------')
+
+if __name__ == '__main__':
+    stations = getData()
+    displayData(stations)
