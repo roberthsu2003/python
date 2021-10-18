cbf09f3d8e103099bdf649085ee860650307092c
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-18 16:58:06 +0800

    modify

--- 職能發展學院/lesson2/lesson2_3.py
+++ 職能發展學院/lesson2/lesson2_3.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 
-window=tk.Tk()
-window.title("Hello Tkinter")
-#window.geometry("500x100")
-label = tk.Label(window,text="Hello World",bg='yellow', fg='#263238', font=('Arial', 20),padx=100,pady=50)
-label.pack(padx=100,pady=50)
-window.mainloop()
+if __name__ == "__main__":
+    window=tk.Tk()
+    window.title("Hello Tkinter")
+    #window.geometry("500x100")
+    label = tk.Label(window,text="Hello World",bg='yellow', fg='#263238', font=('Arial', 20, 'bold', 'italic'),padx=100,pady=50)
+    label.pack(padx=100,pady=50)
+    window.mainloop()
