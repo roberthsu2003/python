297e49785bb37f8d1f50c5262dbef9841dad4893
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-25 15:51:17 +0800

    modify

---
+++ 職能發展學院/lesson4/data/youbikeData.py
@@ -0,0 +1,10 @@
+def get_youbike_data():
+    import requests
+    youbike_url = "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
+    response = requests.get(youbike_url)
+    if response.status_code == 200:
+        print("下載成功")
+        print(response.encoding)
+        print(response.text)
+    else:
+        print("下載失敗")
