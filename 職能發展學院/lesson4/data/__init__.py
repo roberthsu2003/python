c5d7dbdd11ac13cd9ba97111b0b2381129188eb2
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-25 16:41:00 +0800

    modify

--- 職能發展學院/lesson4/data/__init__.py
+++ 職能發展學院/lesson4/data/__init__.py
@@ -1,1 +1,2 @@
 from .youbikeData import get_youbike_data
+from .post_atm_address import get_atm_address
---
+++ 職能發展學院/lesson4/data/post_atm_address.py
@@ -0,0 +1,2 @@
+def get_atm_address():
+    atm_url="https://www.post.gov.tw/post/internet/Templates/getOpenDataFile.jsp?vkey=85422E4D-6133-4404-851E-08EC2077A162"
