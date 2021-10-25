211aa6b587acc7ab24b7ef29f5e0a0d403bc1485
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-25 13:53:27 +0800

    modify

--- 職能發展學院/lesson3/w4.py
+++ 職能發展學院/lesson3/w4.py
@@ -10,7 +10,7 @@ class Window(tk.Tk):
         label = tk.Label(self,textvar=self.label_text,font=("arial",20))
         label.pack(padx=100, pady=50)
 
-        hello_button = tk.Button(self, text="Say Hello",font=("arial", 20),padx=5,pady=5,command=self.say_hello1)
+        hello_button = tk.Button(self, text="Say Hello",font=("arial", 20),padx=5,pady=5,command=self.say_hello)
         hello_button.pack(side=tk.LEFT,padx=20,pady=20)
 
         goodbye_button = tk.Button(self, text="Say Goodbye", font=("arial", 20), padx=5, pady=5,command=self.say_goodbye)
@@ -21,7 +21,7 @@ class Window(tk.Tk):
         self.label_text.set("GoodBye! \n 2秒鐘後")
         self.after(2000, self.destroy)
 
-    def say_hello1(self):
+    def say_hello(self):
         self.label_text.set("Hello! Python!")
 
 if __name__ == "__main__":
