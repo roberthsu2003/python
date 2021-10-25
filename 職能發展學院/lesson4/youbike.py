ff3a0f764f12c43254f19761e1f6e825b93f9f58
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-25 16:53:10 +0800

    modify

--- 職能發展學院/lesson4/youbike.py
+++ 職能發展學院/lesson4/youbike.py
@@ -1,4 +1,5 @@
 import data
 if __name__ == "__main__":
-    youbikeData = data.get_youbike_data();
-    print(youbikeData)
+    #youbikeData = data.get_youbike_data();
+    #print(youbikeData)
+    data.post_atm_address()
