1685634149b7f44cb5c3752258f4855dca68e890
徐國堂 <roberthsu2003@gmail.com>
2022-01-10 16:55:10 +0800

    modify

--- 新尖兵專案/flaskBootstrap/main.py
+++ 新尖兵專案/flaskBootstrap/main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<div style="font-size:2rem;color:red"> Hello! World</div>'
+    return render_template('index.html')
