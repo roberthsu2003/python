3ff4606f0e360272c5a3c3c30a81309c7f491dd2
徐國堂 <roberthsu2003@gmail.com>
2022-01-12 13:32:04 +0800

    modify

---
+++ 新尖兵專案/flaskBootstrap1/main.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/layout')
+@app.route('/layout/box')
+def layout():
+    return render_template('layout.html')
+
+@app.route('/layout/container')
+def container():
+    return  render_template('container.html',name="Robert")
