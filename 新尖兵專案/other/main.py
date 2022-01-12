03909bf6126d99dba181f636a314bff07405c092
徐國堂 <roberthsu2003@gmail.com>
2022-01-12 10:41:09 +0800

    modify

--- 新尖兵專案/other/main.py
+++ 新尖兵專案/other/main.py
@@ -6,3 +6,8 @@ if __name__ == "__main__":
     print(another1.__name__)
     print(tools.__name__)
     print(tools.toos_list)
+    print(tools.add(10, 20))
+    print(tools.Bicycle.add(5,10))
+    bike = tools.get_bicycle()
+    print(bike.__class__)
+    bike.sayHello()
--- 新尖兵專案/other/tools.py
+++ 新尖兵專案/other/tools.py
@@ -1,1 +1,15 @@
 toos_list = list(range(11))
+
+def add(a,b):
+    return a + b
+
+def get_bicycle():
+    return Bicycle()
+
+class Bicycle():
+    def sayHello(self):
+        print("Hello!")
+
+    @staticmethod
+    def add(a,b):
+        return a+b
