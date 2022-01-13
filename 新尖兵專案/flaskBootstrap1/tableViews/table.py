712d7ce455d54d52c4b125a7092f84f297610533
徐國堂 <roberthsu2003@gmail.com>
2022-01-13 15:05:58 +0800

    modify

--- 新尖兵專案/flaskBootstrap1/tableViews/table.py
+++ 新尖兵專案/flaskBootstrap1/tableViews/table.py
@@ -1,6 +1,10 @@
 from flask import Blueprint,render_template
+import requests
 
 tableApp = Blueprint('table_page',__name__)
+apikey = '84a024b0-1d85-460c-b330-d96b9ea12cb9'
+url = 'https://data.epa.gov.tw/api/v1/aqx_p_137?format=csv&offset=0&limit=1000&api_key=84a024b0-1d85-460c-b330-d96b9ea12cb9'
+
 @tableApp.route('/table')
 def table():
     return render_template('table.html',name='table')
