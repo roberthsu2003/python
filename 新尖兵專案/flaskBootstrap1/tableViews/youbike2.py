08ceae1b784318bb512e51030e591aee84fe9eed
徐國堂 <roberthsu2003@gmail.com>
2022-01-14 13:43:51 +0800

    modify

--- 新尖兵專案/flaskBootstrap1/tableViews/youbike2.py
+++ 新尖兵專案/flaskBootstrap1/tableViews/youbike2.py
@@ -1,7 +1,24 @@
 from flask import Blueprint,render_template
+import requests
+from requests import ConnectionError,ConnectTimeout,HTTPError,TooManyRedirects
 
 youbikeApp = Blueprint('youbike',__name__)
+url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v3/youbike_immediate.json'
 
 @youbikeApp.route('/table/youbike')
-def youbike():
+@youbikeApp.route('/table/youbike/<region>')
+def youbike(region):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except ConnectionError as e:
+        print(e)
+    except ConnectTimeout as e:
+        print(e)
+    except HTTPError as e:
+        print(e)
+    except TooManyRedirects as e:
+        print(e)
+    except:
+        print('error')
     return render_template('youbike.html')
