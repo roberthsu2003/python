a41ef5bf4fa6c971332da914788a2778f3c292e0
徐國堂 <roberthsu2003@gmail.com>
2022-01-17 17:46:51 +0800

    modify

--- 新尖兵專案/flaskBootstrap1/sqlViews/loto.py
+++ 新尖兵專案/flaskBootstrap1/sqlViews/loto.py
@@ -2,5 +2,6 @@ from flask import Blueprint,render_template
 sqlApp = Blueprint("sql",__name__)
 
 @sqlApp.route('/sqlalchemy')
+@sqlApp.route('/sqlalchemy/loto')
 def loto():
-    return render_template('loto.html')
+    return render_template('loto.html',name='loto')
