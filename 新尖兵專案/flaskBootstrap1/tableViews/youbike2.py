6fa4fdb5817cfe5ca634eb70df52f1125f4fbc43
徐國堂 <roberthsu2003@gmail.com>
2022-01-17 17:08:01 +0800

    modify

--- 新尖兵專案/flaskBootstrap1/tableViews/youbike2.py
+++ 新尖兵專案/flaskBootstrap1/tableViews/youbike2.py
@@ -88,3 +88,30 @@ def youbike_api():
         dataDict[key] = regionList
 
     return json.dumps(dataDict,ensure_ascii=False)
+
+@youbikeApp.route('/table/youbike/api/<region>')
+def youbike_api1(region):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except ConnectionError as e:
+        abort(500)
+    except ConnectTimeout as e:
+        abort(500)
+    except HTTPError as e:
+        abort(500)
+    except TooManyRedirects as e:
+        abort(500)
+    except:
+        abort(500)
+
+    jsonObject = response.json()
+    sareas = list({siteDict['sarea'] for siteDict in jsonObject})
+    dataDict = dict()
+    for key in sareas:
+        regionList = [item for item in jsonObject if item['sarea'] == key]
+        dataDict[key] = regionList
+
+    dataList = dataDict[region]
+
+    return json.dumps(dataList,ensure_ascii=False)
