500595172b88a9761e973ebe194bda654433d654
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 10:24:47 +0800

    modify

---
+++ 新尖兵專案/javascript/ch01/js/add-content1.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours()
+var greeting;
+
+if(hourNow > 18){
+    greeting = '晚安';
+}else if(hourNow > 12){
+    greeting = '午安';
+}else if(hourNow >0){
+    greeting = "早安"
+}else{
+    greeting = "welcome"
+}
+
+document.write('<h3>' + greeting + '</h3>')
