97191bc6064525fe4c986767216af5f4357fe23d
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 11:22:27 +0800

    modify

---
+++ 新尖兵專案/javascript/ch02/js/string-variable.js
@@ -0,0 +1,11 @@
+var username;
+var message;
+
+username = "徐國堂";
+message = "高富帥,白富美";
+
+var elName = document.getElementById('name');
+var elNote = document.getElementById('note');
+
+elName.innerHTML = username;
+elNote.innerHTML = message;
