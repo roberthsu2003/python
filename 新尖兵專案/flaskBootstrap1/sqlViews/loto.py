23e6ec93a9f9d789334d1d0b0746b5a35dfd32c4
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 15:48:59 +0800

    modify

--- 新尖兵專案/flaskBootstrap1/sqlViews/loto.py
+++ 新尖兵專案/flaskBootstrap1/sqlViews/loto.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 
 sqlApp = Blueprint("sql",__name__)
-engine = create_engine("sqlite:///data.db", echo=True)
+
 Base = declarative_base()
 class Loto(Base):
     __tablename__ = 'loto'
@@ -22,8 +22,9 @@ class Loto(Base):
     def __repr__(self):
         return f"<(日期='{self.日期}', num1={self.num1}, num2={self.num2},num3={self.num3},num4={self.num4},num5={self.num5},num6={self.num6},特別號={self.特別號})>"
 
-Loto(日期=datetime.now(),num1=23, num2=45, num3=13, num4=25, num5=42, num6=3, 特別號=8)
-
+#Loto(日期=datetime.now(),num1=23, num2=45, num3=13, num4=25, num5=42, num6=3, 特別號=8)
+engine = create_engine("sqlite:///sqlViews/data.db", echo=True)
+Base.metadata.create_all(engine)
 
 @sqlApp.route('/sqlalchemy')
 @sqlApp.route('/sqlalchemy/loto')
