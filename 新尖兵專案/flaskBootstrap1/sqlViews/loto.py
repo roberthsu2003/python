b0b4a88e49796f6889352235919705aa73d54edd
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 15:40:34 +0800

    modify

--- 新尖兵專案/flaskBootstrap1/sqlViews/loto.py
+++ 新尖兵專案/flaskBootstrap1/sqlViews/loto.py
@@ -1,5 +1,29 @@
 from flask import Blueprint,render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+
 sqlApp = Blueprint("sql",__name__)
+engine = create_engine("sqlite:///data.db", echo=True)
+Base = declarative_base()
+class Loto(Base):
+    __tablename__ = 'loto'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    日期 = Column(DateTime,nullable=False)
+    num1 = Column(Integer,nullable=False)
+    num2 = Column(Integer, nullable=False)
+    num3 = Column(Integer, nullable=False)
+    num4 = Column(Integer, nullable=False)
+    num5 = Column(Integer, nullable=False)
+    num6 = Column(Integer, nullable=False)
+    特別號 = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<(日期='{self.日期}', num1={self.num1}, num2={self.num2},num3={self.num3},num4={self.num4},num5={self.num5},num6={self.num6},特別號={self.特別號})>"
+
+Loto(日期=datetime.now(),num1=23, num2=45, num3=13, num4=25, num5=42, num6=3, 特別號=8)
+
 
 @sqlApp.route('/sqlalchemy')
 @sqlApp.route('/sqlalchemy/loto')
