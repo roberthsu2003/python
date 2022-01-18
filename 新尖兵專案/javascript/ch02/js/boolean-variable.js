a68d9670eab1809815dcbc91294f6a771a55d996
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 11:49:38 +0800

    modify

---
+++ 新尖兵專案/javascript/ch02/js/boolean-variable.js
@@ -0,0 +1,10 @@
+var inStock;
+var shipping;
+inStock = true;
+shipping = false;
+
+var elStock = document.getElementById('stock');
+var elShip = document.getElementById('shipping');
+
+elStock.classList.add(inStock);
+elShip.classList.add(shipping);
