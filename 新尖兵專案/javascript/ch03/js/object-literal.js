8cc71a4741240e1b6b7e3a83f37db7a8e9aa1bb0
徐國堂 <roberthsu2003@gmail.com>
2022-01-18 13:56:21 +0800

    modify

---
+++ 新尖兵專案/javascript/ch03/js/object-literal.js
@@ -0,0 +1,14 @@
+var hotel = {
+    name: 'Quay',
+    rooms: 40,
+    booked: 25,
+    checkAvailability: function() {
+        return this.rooms - this.booked;
+    }
+};
+
+var elName = document.getElementById('hotelName');
+elName.textContent = hotel.name;
+
+var elRooms = document.getElementById('rooms');
+elRooms.textContent = hotel.checkAvailability();
