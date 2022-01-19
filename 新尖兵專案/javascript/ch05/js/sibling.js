2d1f7bd4a1658d00decbb724fdece13e59357ec0
徐國堂 <roberthsu2003@gmail.com>
2022-01-19 13:45:25 +0800

    modify

---
+++ 新尖兵專案/javascript/ch05/js/sibling.js
@@ -0,0 +1,6 @@
+var startItem  = document.getElementById('two')
+var prevItem = startItem.previousSibling;
+var nextItem  = startItem.nextSibling;
+
+prevItem.className = 'complete'
+nextItem.className = 'cool'
