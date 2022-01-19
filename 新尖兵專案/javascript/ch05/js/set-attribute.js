5dc12d07577e74c81dd07b75530983ea310d9b90
徐國堂 <roberthsu2003@gmail.com>
2022-01-19 14:49:03 +0800

    modify

---
+++ 新尖兵專案/javascript/ch05/js/set-attribute.js
@@ -0,0 +1,6 @@
+var firstItem = document.getElementById('one');
+firstItem.className = 'complete'
+
+var fourthItem = document.getElementsByClassName('li').item(3);
+
+fourthItem.setAttribute('class','cool')
