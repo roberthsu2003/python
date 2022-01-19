c00dbd50408e1f372e56e1ed3cbc7f219b266a32
徐國堂 <roberthsu2003@gmail.com>
2022-01-19 13:59:33 +0800

    modify

---
+++ 新尖兵專案/javascript/ch05/js/child.js
@@ -0,0 +1,6 @@
+var startItem  =  document.getElementsByTagName('ul')[0]
+var firstItem = startItem.firstElementChild;
+var lastItem = startItem.lastElementChild;
+
+firstItem.classList.add('complete');
+lastItem.classList.add('cool');
