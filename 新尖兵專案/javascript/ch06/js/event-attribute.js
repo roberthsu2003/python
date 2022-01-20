4df58a911eb4729051c20dfcfc992281559b7c23
徐國堂 <roberthsu2003@gmail.com>
2022-01-20 10:48:04 +0800

    modify

--- 新尖兵專案/javascript/ch06/js/event-attribute.js
+++ 新尖兵專案/javascript/ch06/js/event-attribute.js
@@ -1,7 +1,7 @@
 function checkUsername(){
     var elMsg = document.getElementById('feedback');
     var elUsername = document.getElementById('username');
-    if(elUsername.nodeValue.length < 5){
+    if(elUsername.value.length < 5){
         elMsg.textContent = "必需要5個字以上"
     }else{
         elMsg.textContent = ""
