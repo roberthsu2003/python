62cac416b37e1ce2906af211792e2f6946bf4855
徐國堂 <roberthsu2003@gmail.com>
2022-01-20 11:08:17 +0800

    modify

---
+++ 新尖兵專案/javascript/ch06/js/event-listener.js
@@ -0,0 +1,22 @@
+/*
+function checkUsername(){
+    var elMsg = document.getElementById('feedback');
+    var elUsername = document.getElementById('username');
+    if(elUsername.value.length < 5){
+        elMsg.textContent = "必需要5個字以上"
+    }else{
+        elMsg.textContent = ""
+    }
+}
+*/
+
+var elUsername = document.getElementById('username');
+elUsername.addEventListener('blur',(event)=>{
+    var elMsg = document.getElementById('feedback');
+    var elUsername = document.getElementById('username');
+    if(elUsername.value.length < 5){
+        elMsg.textContent = "必需要5個字以上"
+    }else{
+        elMsg.textContent = ""
+    }
+});
