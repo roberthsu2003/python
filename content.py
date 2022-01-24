8228892d4c7c807a969e30af08cc112a05748dd9
徐國堂 <roberthsu2003@gmail.com>
2022-01-24 11:57:17 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,23 +1,5 @@
-#單行文字
-poem = "Apple Arcade 推出的最新遊戲《Next Stop Nowhere》，由位於洛杉磯的開發商 Night School Studio 出品。這款遊戲是太空公路之旅的夥伴冒險，其中不同角色間展開了一些精彩的劇情。在銀河系的外圍，每個人和所有事物之間都有充分的空間，Night School 希望這款遊戲可為近來可能感到孤立的玩家提供一些慰藉。"
-poem
-
-
-
-
-#單行文字
-poem = "Apple Arcade 推出的最新遊戲《Next Stop Nowhere》，由位於洛杉磯\
-的開發商 Night School Studio 出品。這款遊戲是太空公路之旅的夥伴冒險，其中\
-不同角色間展開了一些精彩的劇情。在銀河系的外圍，每個人和所有事物之間都有充分\
-的空間，Night School 希望這款遊戲可為近來可能感到孤立的玩家提供一些慰藉。"
-poem
-
-
-
-#多行文字
-poem2 = '''Apple Arcade 推出的最新遊戲《Next Stop Nowhere》，由位於洛杉磯的開發商 Night School Studio 出品。
-
-這款遊戲是太空公路之旅的夥伴冒險，其中不同角色間展開了一些精彩的劇情。
-
-在銀河系的外圍，每個人和所有事物之間都有充分的空間，Night School 希望這款遊戲可為近來可能感到孤立的玩家提供一些慰藉。'''
-print(poem2)
+#輸出 print()
+#輸入 input()
+value1 = input("請輸入一個整數:")
+value2 = input("請輸入另一個整數:")
+int(value1) + int(value2)
