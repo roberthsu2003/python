eb476116f84cbe5b58da01a85e589e2fa165c01c
徐國堂 <roberthsu2003@gmail.com>
2022-01-24 10:35:53 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,4 +1,32 @@
-#下面是print()方法的說明
-print("m:",m)
-print("n",n)
-print("total",total)
+x = 100
+print(type(x))
+
+y = 100.0
+print(type(y))
+
+m = '100.0'
+print(type(m))
+
+
+n = True
+print(type(n))
+
+
+
+
+
+
+
+
+x = 100
+print(type(x))
+
+x = 100.0
+print(type(x))
+
+x = '100.0'
+print(type(x))
+
+
+x = True
+print(type(x))
