3aece7c84340be843ce8c6dab15711b1408a3c31
徐國堂 <roberthsu2003@gmail.com>
2022-01-24 17:08:29 +0800

    modify

--- 新尖兵專案/javascript/ch06/js/event-flow.js
+++ 新尖兵專案/javascript/ch06/js/event-flow.js
@@ -1,13 +1,19 @@
-function showElement(){
-    alert(this.innerHTML);
-}
+
 
 
 el = document.getElementById("list");
-el.addEventListener('click',showElement,false);
+el.addEventListener('click',(event) => {
+    alert(event.currentTarget.innerHTML);
+},false);
 
 el1 = document.getElementById('item');
-el1.addEventListener('click',showElement,false);
+el1.addEventListener('click',(event) => {
+    alert(event.currentTarget.innerHTML);
+},false);
 
-el2 = document.getElementsById('link');
-el2.addEventListener('click',showElement,false);
+el2 = document.getElementById('link');
+el2.addEventListener('click',(event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    alert(event.currentTarget.innerHTML);
+},false);
