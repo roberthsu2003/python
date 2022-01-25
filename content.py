3fc5c879e45e9010783f7e1588f6218e3786f865
徐國堂 <roberthsu2003@gmail.com>
2022-01-25 10:16:36 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,5 +1,5 @@
-top = float(input('請輸入梯形的上底(公分):'))
-bottom = float(input('請輸入梯形的下底(公分):'))
-height = float(input('請輸入梯形的高(公分):'))
-area = (top + bottom) * height / 2
-print('梯形的面積:',area,'平方公分')
+a = 10
+if a >= 14:
+  a -= 2
+  print(a)
+print('程式結束')
