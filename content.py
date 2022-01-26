a047b89566b4125eaf0b625053c99308c145d4b7
徐國堂 <roberthsu2003@gmail.com>
2022-01-26 11:17:02 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,10 +1,12 @@
-num = 0
 sum = 0
+num = 00
 while(True):
   num += 1
-  score = int(input('請輸入第' + str(num) + '位學生的成績:'))
-  if(score < 0):
+  inputNum = int(input("請輸入第" + str(num) + "個數值:"))
+  if(inputNum < 0):
     break
-  sum += score
-
-print("全班總成績為:%d,平均分數為:%.2f" % (sum, sum/(num-1)))
+  elif(inputNum % 2 == 1):
+    continue
+  else:
+    sum += inputNum
+print(f"所有輸入的正偶數的加總是:{sum}")
