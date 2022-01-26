6bf53a4b15b816366cd935a02037bafb4703362a
徐國堂 <roberthsu2003@gmail.com>
2022-01-26 09:45:38 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,14 +1,6 @@
-score = 56
-
-if score <= 90 and score > 80:
-  print('80~90')
-else:
-  print('其它')
-  
-  
-
- score = 87
-if 90 >= score > 80:
-   print('80~90')
-else:
-  print('其它')
+#使用while迴圈,執行明確的次數,3個步驟
+a = 5
+while a > 0:
+  print(a)
+  a -= 1
+print("程式結束")
