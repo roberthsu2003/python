d9a22551de08065b7606c8b57b19f88cca42dcbd
徐國堂 <roberthsu2003@gmail.com>
2022-01-26 11:43:15 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,12 +1,19 @@
-sum = 0
-num = 00
+import random
+min = 1
+max = 20
+count = 0
+target = random.randint(min,max)
+print("==========猜數字遊戲================:\n\n")
+
 while(True):
-  num += 1
-  inputNum = int(input("請輸入第" + str(num) + "個數值:"))
-  if(inputNum < 0):
+  count += 1
+  keyin =int(input("猜數字範圍{0}~{1}:".format(min,max)))
+  if(keyin == target):
+    print(f"賓果!猜對了, 答案是:{keyin}")
+    print(f"您猜了{count}次")
     break
-  elif(inputNum % 2 == 1):
-    continue
-  else:
-    sum += inputNum
-print(f"所有輸入的正偶數的加總是:{sum}")
+  elif(keyin > target):
+    print("再小一點")
+  elif(keyin < target):
+    print("再大一點")
+print("Game Over")
