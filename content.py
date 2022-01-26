696f7b1c1cdcd2725b8a08a490ec2abb8352a005
徐國堂 <roberthsu2003@gmail.com>
2022-01-26 10:17:19 +0800

    Update content.py

--- content.py
+++ content.py
@@ -3,10 +3,15 @@ i = 2
 count = 0
 while i <= 10:
   count += 1
-  total += i;
+  total += i
   #print("第",count,"次迴圈的i=", i,"總和為", total)
   #舊的方法
-  print("第%d次迴圈的i=%i總和為%i" % (count, i, total))
+  #print("第%d次迴圈的i=%i總和為%i" % (count, i, total))
+
+  #新的方法
+  print("第{:d}次迴圈的i={:d}總和為{:.2f}".format(count, i, total))
+
+  
   i += 2
 print('程式結束')
 
