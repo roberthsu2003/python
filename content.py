65f23700c6e1c794e2ac6891aba6fc69567d29be
徐國堂 <roberthsu2003@gmail.com>
2022-01-26 10:48:52 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,19 +1,8 @@
-total = 0
-i = 2
-count = 0
-while i <= 10:
-  count += 1
-  total += i
-  #print("第",count,"次迴圈的i=", i,"總和為", total)
-  #舊的方法
-  #print("第%d次迴圈的i=%i總和為%i" % (count, i, total))
+deposit = 0
+num = 0
+while(deposit < 30000):
+  num += 1
+  inputValue = int(input(f"請輸入第{num}月份的存款:"))
+  deposit += inputValue
 
-  #新的方法
-  #print("第{:d}次迴圈的i={:d}總和為{:.2f}".format(count, i, total))
-
-  #字串插補
-  print(f"第{count}次迴圈的i={i}總和為{total}")
-
-  i += 2
-print('程式結束')
-
+print("恭喜!已經存夠了，存了{:d}月,共存了{:.2f}元".format(num,deposit))
