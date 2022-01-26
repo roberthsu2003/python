64669bfbc268507bf13ce98091d4d1d10f8c28e2
徐國堂 <roberthsu2003@gmail.com>
2022-01-26 10:08:26 +0800

    Update content.py

--- content.py
+++ content.py
@@ -4,6 +4,9 @@ count = 0
 while i <= 10:
   count += 1
   total += i;
-  print("第",count,"次迴圈的i=", i,"總和為", total);
+  #print("第",count,"次迴圈的i=", i,"總和為", total)
+  #舊的方法
+  print("第%d次迴圈的i=%i總和為%i" % (count, i, total))
   i += 2
 print('程式結束')
+
