7a6477ed7d203ecfdc4402b0e95d7b309a6f0067
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 12:00:18 +0800

    Update content.py

--- content.py
+++ content.py
@@ -6,4 +6,11 @@ for i in range(50):
     scores.append(random.randint(50,100))
   students.append(scores)
 
-students
+for student in students:
+  print(student,end=',')
+  total = 0
+  for num in student:
+    total += num
+  
+  print(total,end=',')
+  print(total/5.0,end='\n')
