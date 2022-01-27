f5f653a2b53f2778d87cbd0d5ff35e5b0c1cb116
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 09:30:21 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,3 +1,7 @@
-letters = 'abcdefghijklmnopqrstuvwxys'
-for word in letters:
-  print(word)
+greeting = "Hello!"
+type(greeting)
+len(greeting)
+print(greeting[5])
+print(greeting[-1])
+print(greeting[0])
+print(greeting[5])
