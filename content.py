1971ac6b64964bd04f41667b331bb6b3c43261b4
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 10:06:12 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,4 +1,4 @@
-name = "Henny" #不可變的串列資料
-newName = name.replace('H',"P") #replace()字串的方法
-print(name)
-print(newName)
+letters = 'abcdefghijklmnopqrstuvwxyz'
+print(letters[:])
+print(letters[::2])
+print(letters[::-1])
