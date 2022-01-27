dc02b95ce5d1a76cdbdd983c1fe46bb7239e0cd1
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 10:29:11 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,13 +1,13 @@
-#list
+#tuple
 #for in
 #索引編號取出值
-#可以改內容的串列
-list_num = [10,20,30,40,50,60]
+#不可以改內容的串列
+list_num = (10,20,30,40,50,60)
 for num in list_num:
   print(num)
 print(list_num[0])
 print(list_num[:3])
 print(list_num[-3:])
-list_num[0] = 100
-list_num[1] = 200
+#list_num[0] = 100 #tuple不可以改變內容
+#list_num[1] = 200 #tuple不可以改變內容
 print(list_num)
