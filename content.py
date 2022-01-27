3d350a0f172d9d959a70f078662434a2e5aa090d
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 10:23:14 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,4 +1,13 @@
-letters = 'abcdefghijklmnopqrstuvwxyz'
-print(letters[:])
-print(letters[::2])
-print(letters[::-1])
+#list
+#for in
+#索引編號取出值
+#可以改內容的串列
+list_num = [10,20,30,40,50,60]
+for num in list_num:
+  print(num)
+print(list_num[0])
+print(list_num[:3])
+print(list_num[-3:])
+list_num[0] = 100
+list_num[1] = 200
+print(list_num)
