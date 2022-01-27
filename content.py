890983a463e0727e8d8f1350ddc219933eb27eb2
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 09:41:52 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,7 +1,8 @@
-greeting = "Hello!"
-type(greeting)
-len(greeting)
-print(greeting[5])
-print(greeting[-1])
-print(greeting[0])
-print(greeting[5])
+myString = "Hello World"
+print(myString[0])
+print(myString[0:3])
+print(myString[-11:-8])
+print(myString[:3])
+print(myString[6:])
+print(myString[:5])
+print(myString[-5:])
