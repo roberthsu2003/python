033fb3e341d413bbda53c97545a7d15eec23aad0
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 11:11:08 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,13 +1,6 @@
-#tuple
-#for in
-#索引編號取出值
-#不可以改內容的串列
-list_num = (10,20,30,40,50,60)
-for num in list_num:
-  print(num)
-print(list_num[0])
-print(list_num[:3])
-print(list_num[-3:])
-#list_num[0] = 100 #tuple不可以改變內容
-#list_num[1] = 200 #tuple不可以改變內容
-print(list_num)
+empty_list = []
+print(empty_list)
+another_list = list()
+print(another_list)
+weekdays =  ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'] 
+print(weekdays)
