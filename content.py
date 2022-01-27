0fb6530700d70465135f0b65d82c967173eb1806
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 11:12:01 +0800

    Update content.py

--- content.py
+++ content.py
@@ -4,3 +4,18 @@ another_list = list()
 print(another_list)
 weekdays =  ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'] 
 print(weekdays)
+
+
+
+#字串轉為list
+lcat = list('cat')
+lcat
+
+lnums = list(range(1,100,5))
+print(lnums)
+
+#list可以相加
+num1 = [1, 3, 5]
+num2 = [2, 4, 6]
+num12 = num1 + num2
+num12
