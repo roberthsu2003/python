707b5a9b75d4b1d6914f8c532a01b4fe8022b7e0
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 11:50:49 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,26 +1,9 @@
-numbers = [10, 20, 30, 40]
-print(numbers)
-numbers.append(50)
-print(numbers)
-numbers.extend([60, 70])
-print(numbers)
-numbers += [80, 90]
-print(numbers)
-numbers.insert(1,15)
-print(numbers)
+import random
+students = []
+for i in range(50):
+  scores = []
+  for j in range(5):
+    scores.append(random.randint(50,100))
+  students.append(scores)
 
-
-
-
-
-numbers = [10, 15, 20, 30, 40, 50, 60, 70, 80, 90]
-
-#刪除
-del numbers[1]
-print(numbers)
-
-#pop
-numbers.pop()
-print(numbers)
-numbers.pop(0)
-print(numbers)
+students
