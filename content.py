38668fc7b5805dd66454856eb54d4dfe339db93a
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 11:19:40 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,21 +1,9 @@
-empty_list = []
-print(empty_list)
-another_list = list()
-print(another_list)
-weekdays =  ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'] 
-print(weekdays)
+#1維list
+nb = ['筆記電腦', '商用筆電']
+mobile = ['APPLE', 'ASUS', 'HTC'] 
+home = [3, '冰箱', 2, '洗衣機']
 
-
-
-#字串轉為list
-lcat = list('cat')
-lcat
-
-lnums = list(range(1,100,5))
-print(lnums)
-
-#list可以相加
-num1 = [1, 3, 5]
-num2 = [2, 4, 6]
-num12 = num1 + num2
-num12
+#2維list
+all3c = [nb, mobile, home]
+print(all3c)
+print(all3c[2][3])
