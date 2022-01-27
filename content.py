649d14f2922b942e048e69d9ceba2d2695e16d29
徐國堂 <roberthsu2003@gmail.com>
2022-01-27 09:58:05 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,8 +1,4 @@
-myString = "Hello World"
-print(myString[0])
-print(myString[0:3])
-print(myString[-11:-8])
-print(myString[:3])
-print(myString[6:])
-print(myString[:5])
-print(myString[-5:])
+name = "Henny" #不可變的串列資料
+newName = name.replace('H',"P") #replace()字串的方法
+print(name)
+print(newName)
