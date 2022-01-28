6ffecac85d654388ea055156d877900a36ab8eb5
徐國堂 <roberthsu2003@gmail.com>
2022-01-28 10:27:28 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,16 +1,18 @@
-#keys(),values(),items()
-signals = {'綠燈': '走', '黃燈': '走快些', '紅燈': '停等'} 
-print(signals.keys())
+#set
+empty_set = set()
+print(empty_set)
 
-print(signals.values())
+even_numbers = {0, 2, 4, 6, 8}
+print(even_numbers)
 
-print(signals.items())
+#元素不會重覆
+odd_numbers = {1, 1, 1, 3, 3, 3, 5, 5, 5, 7, 7, 7, 9, 9, 9}
+print(odd_numbers)
 
+#新增元素add()
 
+odd_numbers.add(11)
+print(odd_numbers)
 
-
-
-
-#for in取出key和value
-for key,value in signals.items():
-  print(key,value)
+for item in list(odd_numbers):
+  print(item)
