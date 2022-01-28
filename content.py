27862ba814eb365a8b6588eb57051f04a99677b9
徐國堂 <roberthsu2003@gmail.com>
2022-01-28 11:59:01 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,4 +1,6 @@
-print(type(+5E10))
-print(type(4.0))
-print(type("False"))
-print(type(True))
+datas = {1: '張三', 2: '李四', 3: '王五'}
+num = int(input('請輸入座號: '))
+if not num in datas:
+  print('該座號不存在！')
+else:
+  print("學生姓名為： " + datas[num])
