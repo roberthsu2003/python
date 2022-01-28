2021ba5c55c52d733472d9f12315535150c81c4e
徐國堂 <roberthsu2003@gmail.com>
2022-01-28 10:57:13 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,18 +1,9 @@
-#set
-empty_set = set()
-print(empty_set)
+#大樂透電腦選號
+import random
+lot = set()
+while(len(lot)<7):
+  lot.add(random.randint(1,49))
 
-even_numbers = {0, 2, 4, 6, 8}
-print(even_numbers)
-
-#元素不會重覆
-odd_numbers = {1, 1, 1, 3, 3, 3, 5, 5, 5, 7, 7, 7, 9, 9, 9}
-print(odd_numbers)
-
-#新增元素add()
-
-odd_numbers.add(11)
-print(odd_numbers)
-
-for item in list(odd_numbers):
-  print(item)
+for i in list(lot)[:6]:
+  print(i,end=' ')
+print(f"特別號:{list(lot)[-1]}")
