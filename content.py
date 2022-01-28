282d59fd5c2b81ad31c1c1390f01ce784eecc69e
徐國堂 <roberthsu2003@gmail.com>
2022-01-28 09:35:09 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,8 +1,21 @@
-#list的方法index()
-cities =  ['台北', '台中', '高雄', '台南', '花蓮']
-cities.index('台中')
-element = '台南'
-if element in cities:
-  print(f'{element}的索引編號是{cities.index(element)}')
-else:
-  print(element)
+marxes = ['Groucho', 'Chico', 'Harpo']
+sorted_marxes = sorted(marxes) #由小到大
+sorted_marxes
+
+
+
+marxes = ['Groucho', 'Chico', 'Harpo']
+sorted_marxes = sorted(marxes,reverse=True) #由大到小
+sorted_marxes
+
+
+#list的方法sort(),不會傳出，只會排序自已的內容
+marxes = ['Groucho', 'Chico', 'Harpo']
+marxes.sort() #由小到大
+marxes
+
+
+#list的方法sort(),不會傳出，只會排序自已的內容
+marxes = ['Groucho', 'Chico', 'Harpo']
+marxes.sort(reverse=True) #由大到小
+marxes
