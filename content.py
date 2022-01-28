da143442fd287161e0176928ef23647bd999821d
徐國堂 <roberthsu2003@gmail.com>
2022-01-28 11:50:26 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,12 +1,4 @@
-x1 = '5'
-y1 = 4
-a = x1 * y1
-print(type(a))
-
-x2, y2 = 6, 3
-b = x2 / y2
-print(type(b))
-
-x3, y3 = 1.0, 2
-c = x3 + y3
-print(type(c))
+print(type(+5E10))
+print(type(4.0))
+print(type("False"))
+print(type(True))
