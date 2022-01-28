4458079af56aa623a261d1366e8e1e5a915da71d
徐國堂 <roberthsu2003@gmail.com>
2022-01-28 11:43:39 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,5 +1,12 @@
-born = input("請輸入出生的民國年： ")
-year = input("請輸入現在的民國年： ")
-age = eval(year) - eval(born)
-msg = "你的年齡是： " + str(age)
-print(msg)
+x1 = '5'
+y1 = 4
+a = x1 * y1
+print(type(a))
+
+x2, y2 = 6, 3
+b = x2 / y2
+print(type(b))
+
+x3, y3 = 1.0, 2
+c = x3 + y3
+print(type(c))
