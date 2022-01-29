7db3abfea4088d70c2941108a043047635c4bf8a
徐國堂 <roberthsu2003@gmail.com>
2022-01-29 09:15:22 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,6 +1,6 @@
-datas = {1: '張三', 2: '李四', 3: '王五'}
-num = int(input('請輸入座號: '))
-if not num in datas:
-  print('該座號不存在！')
-else:
-  print("學生姓名為： " + datas[num])
+#建立一個自訂的function
+def sayHello():
+  print("Hello! Python")
+
+#呼叫function
+sayHello()
