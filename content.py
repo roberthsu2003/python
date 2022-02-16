b8ac7954e8395e670fb0a4287fc9a12c1d30d9d4
徐國堂 <roberthsu2003@gmail.com>
2022-02-16 20:51:36 +0800

    Update content.py

--- content.py
+++ content.py
@@ -2,5 +2,5 @@ chinese = 78
 english = 98
 math = 78
 
-print("國文:",chinese, "英文:", english, "數學:", math)
-print("總分:", chinese + english + math, " 平均:", (chinese + english + math) / 3)
+print("國文:",chinese, " 英文:", english, " 數學:", math, sep='')
+print("總分:", chinese + english + math, " 平均:", (chinese + english + math) / 3, sep='')
