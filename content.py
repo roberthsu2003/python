8a947a50eba1d8777e8c91edf56109fcd06c61cf
徐國堂 <roberthsu2003@gmail.com>
2022-02-16 19:40:48 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,15 +1,1 @@
-word = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-print(word[3:15])
-print(word[3:15:3])
-print(word[3:15:-3])
-print(word[15:3:-3])
-print(word[15:3])
-print(word[::-3])
-
-
-
-
-
-students = list(range(1,101))
-students[0:-15]
-students[:-15]
+print("Hello! Python")
