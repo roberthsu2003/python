62c399cd6f67113d8aa862525bc8d45d72fa0e13
徐國堂 <roberthsu2003@gmail.com>
2022-02-16 21:27:02 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,10 +1,10 @@
-#依據內容,指定型別
-var1 = 20
-var2 = 20.00
-var3 = "abc"
-var4 = True
+a = 5 + 10
 
-print(type(var1))
-print(type(var2))
-print(type(var3))
-print(type(var4))
+b = 2 + 3 * 4
+b
+
+c = (2+3) * 4
+c
+
+d = 2 * (1 + 2) ** 2 - 2 ** 2 * 2
+d
