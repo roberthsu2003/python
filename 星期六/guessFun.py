f5c803df7bae0af0bba5110bfc0baf928dc98ac9
徐國堂 <roberthsu2003@gmail.com>
2022-02-19 16:27:17 +0800

    modify

--- 星期六/guessFun.py
+++ 星期六/guessFun.py
@@ -1,9 +1,8 @@
 import random
 min = 1
-max = 10
+max = 100
 count = 0
 target = random.randint(min, max)
-print(target)
 print("＝＝＝＝＝＝＝＝＝猜數字遊戲===============:\n\n")
 while(True):
    count += 1
@@ -13,8 +12,14 @@ while(True):
            print(f"賓果!猜對了, 答案是:{target}")
            print(f"您猜了{count}次")
            break;
-       else:
-           print(f"您已經猜了{count}次")
+       elif keyin > target:
+           max = keyin
+           print("再小一點")
+       elif keyin < target:
+           min = keyin
+           print("再大一點")
+       print(f"您已經猜了{count}次")
+
    else:
        print(f"請輸入提示範圍內的數字{min}~{max}")
 print("Game Over")
