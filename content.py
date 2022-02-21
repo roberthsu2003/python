e7d00e5ab88e28c8f46b7402b37a20a184794624
徐國堂 <roberthsu2003@gmail.com>
2022-02-21 20:08:12 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,3 +1,4 @@
-n = int(input('請輸入被除數(整數):'))
-m = int(input('請輸入除數(整數,不可以為0)'))
-print(f'商:{n // m} 餘數:{n % m}')
+money = 50000
+cell = int(input("請輸入手機的金額:"))
+money -= cell #複合指定運算子
+print(f'剩餘:{money}')
