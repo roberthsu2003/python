744e3937efda36e5e01ab71e045fc1aaa68f5fa8
徐國堂 <roberthsu2003@gmail.com>
2022-03-12 11:17:33 +0800

    modify

--- 星期六/lesson7_3.py
+++ 星期六/lesson7_3.py
@@ -1,5 +1,9 @@
 import game
 if __name__ == "__main__":
     print("＝＝＝＝＝＝＝＝＝猜數字遊戲===============:\n\n")
-    game.guess_num()
+    while(True):
+        game.guess_num()
+        playagain = input("還要繼續嗎?(y,n)")
+        if playagain == 'n':
+            break
     print("GameOver")
