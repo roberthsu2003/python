2118b54b5ddea42c7e48439ae0da67686bc779e5
徐國堂 <roberthsu2003@gmail.com>
2022-03-19 10:27:16 +0800

    modify

---
+++ 星期六/lesson9_2/main.py
@@ -0,0 +1,9 @@
+from random import choice
+def get_week():
+    return choice(['星期1', '星期2', '星期3', '星期4', '星期5', '星期6', '星期日'])
+
+if __name__ == "__main__":
+    print("小專案lesson9_2")
+    print(get_week())
+
+
