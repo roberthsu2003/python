20f636e32cbf067a1d90f5552bd0700ea9b0e035
徐國堂 <roberthsu2003@gmail.com>
2022-03-19 10:55:19 +0800

    modify

--- 星期六/lesson9_2/main.py
+++ 星期六/lesson9_2/main.py
@@ -1,11 +1,8 @@
-from random import choice
 import tools
-def get_week():
-    return choice(['星期1', '星期2', '星期3', '星期4', '星期5', '星期6', '星期日'])
 
 if __name__ == "__main__":
     print("小專案lesson9_2")
     if tools.OK:
-        print(get_week())
+        print(tools.get_week())
 
 
--- 星期六/lesson9_2/tools.py
+++ 星期六/lesson9_2/tools.py
@@ -1,4 +1,8 @@
+from random import choice
 print("tools module")
 print(f"module的名稱是{__name__}")
 
 OK = True
+
+def get_week():
+    return choice(['星期1', '星期2', '星期3', '星期4', '星期5', '星期6', '星期日'])
