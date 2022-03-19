94fe4ad5a026a857439281066d347e924b743169
徐國堂 <roberthsu2003@gmail.com>
2022-03-19 14:40:20 +0800

    modify

--- 星期六/lesson10_1/main.py
+++ 星期六/lesson10_1/main.py
@@ -1,5 +1,6 @@
 #from tools import game
-from tools.game import guess_num
+#from tools.game import guess_num
+from tools import weather
 
 if __name__ == "__main__":
-    guess_num()
+    weather.get_weather_of_taiwan()
---
+++ 星期六/lesson10_1/tools/weather.py
@@ -0,0 +1,6 @@
+import requests
+
+urlPathApi = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=rdec-key-123-45678-011121314&format=JSON"
+
+def get_weather_of_taiwan():
+    print("get_weather")
