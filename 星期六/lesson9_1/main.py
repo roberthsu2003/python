be7f5e723c591dd74fc888ad2deca1e908dd752d
徐國堂 <roberthsu2003@gmail.com>
2022-03-19 10:16:02 +0800

    modify

--- 星期六/lesson9_1/main.py
+++ 星期六/lesson9_1/main.py
@@ -1,7 +1,9 @@
 #import random as rd
-from random import randrange,randint
+#from random import randrange,randint,choice
+from random import *
 
 if __name__ == "__main__":
     print(randrange(10))
     print(randint(100,105))
+    print(choice(['上午','中午','下午','晚上']))
     print("我是主程式")
