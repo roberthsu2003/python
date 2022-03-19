712650f347836907b9dca65ceebf413bb6e9ae8a
徐國堂 <roberthsu2003@gmail.com>
2022-03-19 15:52:56 +0800

    modify

--- 星期六/lesson10_1/tools/weather.py
+++ 星期六/lesson10_1/tools/weather.py
@@ -3,19 +3,21 @@ import requests
 urlPathApi = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=rdec-key-123-45678-011121314&format=JSON"
 
 def get_weather_of_taiwan():
-    mylist = [10, 20, 30, 40]
     try:
-        mylist[5]
-    except IndexError as e:
+        response = requests.get(urlPathApi)
+        response.raise_for_status()
+    except requests.ConnectionError as e:
         print(e)
-    except:
-        print("mylist[5]出現不明的錯誤!")
-
-    response = requests.get(urlPathApi)
-    if response.status_code == 200:
-        print('下載成功')
-    else:
-        print('下載失敗')
+        return None
+    except requests.HTTPError as e:
+        print(e)
+        return None
+    except requests.Timeout as e:
+        print(e)
+        return None
+    except requests.exceptions.RequestException as e:
+        print(e)
         return None
 
-    return response.json()
+    allData = response.json()
+    return allData
