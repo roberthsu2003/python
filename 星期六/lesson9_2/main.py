81e8fc1beaaeec1f30619ad1aeae6b3f6ac5c60b
徐國堂 <roberthsu2003@gmail.com>
2022-03-19 11:09:32 +0800

    modify

--- 星期六/lesson9_2/main.py
+++ 星期六/lesson9_2/main.py
@@ -1,8 +1,17 @@
 import tools
+from tools import Student
 
 if __name__ == "__main__":
     print("小專案lesson9_2")
     if tools.OK:
         print(tools.get_week())
+
+    stu1 = Student("robert",chinese=89,math=95,english=78)
+    print(stu1)
+    print(f"stu1學生的姓名{stu1.name}")
+    print(f"stu1國文分數:{stu1.chinese}")
+    print(f"stu1英文分數:{stu1.english}")
+    print(f"stu1數學分數:{stu1.math}")
+
 
 
--- 星期六/lesson9_2/tools.py
+++ 星期六/lesson9_2/tools.py
@@ -6,3 +6,10 @@ OK = True
 
 def get_week():
     return choice(['星期1', '星期2', '星期3', '星期4', '星期5', '星期6', '星期日'])
+
+class Student():
+    def __init__(self,name,chinese,english,math):
+        self.name = name
+        self.chinese = chinese
+        self.english = english
+        self.math = math
