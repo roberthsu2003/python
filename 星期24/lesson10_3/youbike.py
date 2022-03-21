e21cc18d3926c33ada234ae4b082fb41a2ee3216
徐國堂 <roberthsu2003@gmail.com>
2022-03-21 21:02:49 +0800

    modify

--- 星期24/lesson10_3/youbike.py
+++ 星期24/lesson10_3/youbike.py
@@ -1,2 +1,9 @@
 def get_youbike_info():
+    list1 = [10, 20, 30, 40]
+    try:
+        print(list1[5])
+    except IndexError as e:
+        print(e)
+    except:
+        print("other Error")
     return "info"
