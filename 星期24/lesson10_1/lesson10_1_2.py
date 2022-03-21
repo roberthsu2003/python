1e8f5d3ef81c5f58d827a4d312554fcb9144ade2
徐國堂 <roberthsu2003@gmail.com>
2022-03-21 19:32:19 +0800

    modify

--- 星期24/lesson10_1/lesson10_1_2.py
+++ 星期24/lesson10_1/lesson10_1_2.py
@@ -1,10 +1,12 @@
 #內建的module(模組)和package(套件)
+import random
 import random as rd
 from random import choice,randint
 #from random import choice as ci
+#from random import *
 
 def getWeek():
     return choice(["星期一","星期二","星期三","星期四","星期五","星期六","星期日"])
 
-print(getWeek)
-print(getWeek())
+if __name__ == "__main__":
+    print(getWeek())
