f4a9b0fd9980ab03923d9854004733ce40fefe20
徐國堂 <roberthsu2003@gmail.com>
2022-06-02 20:17:19 +0800

    modify

--- 星期24/lesson8_3.py
+++ 星期24/lesson8_3.py
@@ -3,17 +3,26 @@ def fun1():
     #function也是一個命名空間
     print("我是fun1")
     print(f"我的名字是{fun1.__name__}")
+    chinese = 90
+    print(f"func1_chinese:{chinese}")
 
 def fun2():
     # function也是一個命名空間
     print("我是fun2")
     print(f"我的名字是{fun2.__name__}")
+    chinese = 80
+    print(f"fun2_chinese:{chinese}")
 
 
 def main():
     print("我是主程式")
     fun1()
     fun2()
+    print(f"chinese:{chinese}")
+
+
 
 if __name__ == "__main__":
+    chinese = 100
+    print(f"__main__ chinese:{chinese}")
     main()
