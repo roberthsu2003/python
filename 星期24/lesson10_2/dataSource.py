b6ed1b16c1c1b0a0cdad327bedfdbaedc26a1ee6
徐國堂 <roberthsu2003@gmail.com>
2022-06-09 21:15:18 +0800

    modify

--- 星期24/lesson10_2/dataSource.py
+++ 星期24/lesson10_2/dataSource.py
@@ -2,16 +2,19 @@ import requests
 import csv
 
 url = "https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=642de4f4-cfdf-4c4c-a2f3-e6e6fe1d5343&rid=7d1c0b7a-ba82-445f-ba7c-563d8014b689"
+filename = "桃園youbike.csv"
 
 def downloadData():
-    filename = "桃園youbike.csv"
     response = requests.get(url, stream=True)
     if response.status_code == 200:
-        file = open(filename,'wb')
-        for chunk in response.iter_content(chunk_size=128):
-            file.write(chunk)
-        file.close()
+        with open(filename,'wb') as file:
+            for chunk in response.iter_content(chunk_size=128):
+                file.write(chunk)
+
 
 
 def parseData():
-    pass
+    with open(filename,'r',newline='') as file:
+        reader = csv.reader(file)
+        csvData = list(reader)
+    return csvData
--- 星期24/lesson10_2/main.py
+++ 星期24/lesson10_2/main.py
@@ -1,8 +1,10 @@
 import dataSource
 
 def main():
-    dataSource.downloadData()
-    dataSource.parseData()
+    dataSource.downloadData() #下載資料
+    data = dataSource.parseData() #解析資料
+    for item in data:
+        print(item)
 
 if __name__ == "__main__":
     main()
