1902b58a93ff67125059b020fad5ae9cf59f9085
徐國堂 <roberthsu2003@gmail.com>
2022-06-09 19:51:56 +0800

    modify

--- 星期24/lesson10_1/main.py
+++ 星期24/lesson10_1/main.py
@@ -3,6 +3,9 @@ import tools
 def main():
     print(f"PI的內容是{tools.PI}")
     print(f"3+4={tools.add(3,4)}")
+    stu1 = tools.getStudent()
+    print(stu1.name)
+    print(stu1.sum())
 
 if __name__ == "__main__":
     main()
