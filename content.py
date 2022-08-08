19e1fc5f40c4410d16734a7a9fe7c7fe38bebd1a
徐國堂 <roberthsu2003@gmail.com>
2022-08-08 15:35:51 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,4 +1,5 @@
-express = input("請輸入運算式")
-answer = eval(input(f"{express}="))
-print(f"您的答案是{answer}")
-print(f"正確答案是:{eval(express)}")
+#角度求弳度
+pi = 3.141592653
+angle = eval(input("請輸入角度:"))
+radian = pi/180 * angle
+print(f"角度:{angle},弳度為:{radian}")
