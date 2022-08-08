05ad293e063f6f7630e1ab65e4706b8bdf037736
徐國堂 <roberthsu2003@gmail.com>
2022-08-08 10:56:28 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,11 +1,1 @@
-sum = 0.0
-value = float(input("請輸入第一個數:"))
-sum += value
-
-value = float(input("請輸入第二個數:"))
-sum += value
-
-value = float(input("請輸入第三個數:"))
-sum += value
-
-print(f"三個數的總和為:{sum}")
+print('Hello! Python')
