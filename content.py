88b7b1ffc624b6f41118f9cb308e3d6e469a21af
徐國堂 <roberthsu2003@gmail.com>
2022-08-08 16:37:14 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,6 +1,11 @@
-score = eval(input("請輸入學生分數:"))
-if score >= 60:
-  print("及格")
+score = int(input("請輸入學生分數:"))
+if score >= 90:
+  print("優")
+elif score >= 80:
+  print("甲")
+elif score >= 70:
+  print("乙")
+elif score >= 60:
+  print("丙")
 else:
-  print("不及格")
-print("結束程式")
+  print("丁")
