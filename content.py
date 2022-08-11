c14b9de39bbef701e377e3321e3dc74146b6a72f
徐國堂 <roberthsu2003@gmail.com>
2022-08-11 14:31:47 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,8 +1,8 @@
-#使用while迴圈建立明確知道執行的次數
-total = 0
-nums = 1
-while nums <=5:
-  value = eval(input(f"請輸入第{nums}位學生的成績:"))
-  total += value
-  nums += 1
-print(f"五位學生的總分為:{total},平均為{total/5:.2f}")
+#小明想要存錢買一輛機車,機車每輛30000元，他將每月存的錢輸入，當存款足夠買機車時，就顯示提示訊息告知。
+#不明確執行幾次
+
+deposit = 0
+while deposit < 30000:
+  value = eval(input("請輸入本月將存款的金額:"))
+  deposit += value
+print(f"您的總存款為{deposit},已經足夠買機車了。")
