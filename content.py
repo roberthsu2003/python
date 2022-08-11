d7ee2db50758ae428dc38818e58e02fef5cbe44b
徐國堂 <roberthsu2003@gmail.com>
2022-08-11 14:57:01 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,8 +1,11 @@
-#小明想要存錢買一輛機車,機車每輛30000元，他將每月存的錢輸入，當存款足夠買機車時，就顯示提示訊息告知。
-#不明確執行幾次
+##小美是一位教師，請你以while迴圈方式為小美設計一個輸入成績的程式，如果輸入負數表示成績輸入結束，在輸入成績結束後顯示班上總成績及平均成績。
+score = 0
+nums = 0
+total = 0
+while score >= 0:  
+  score=eval(input(f"請輸入第{nums+1}學生的分數:"))
+  if score >= 0:
+    nums += 1
+    total += score
 
-deposit = 0
-while deposit < 30000:
-  value = eval(input("請輸入本月將存款的金額:"))
-  deposit += value
-print(f"您的總存款為{deposit},已經足夠買機車了。")
+print(f"學生的數量是{nums},總分是{total},平均是{total/nums:.2f}")
