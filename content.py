b8b6c1723e97cc666b42716a6107553568c8ceb7
徐國堂 <roberthsu2003@gmail.com>
2022-08-16 21:44:49 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,14 +1,9 @@
-#字串的加法運算
-"abc" + "efg"
-
-#字串+數值 -> 出錯
-#"abc" + 13
-
-#str() -> 轉為字串
-"abc" + str(13)
-
-#int() -> 轉為整數
-int("13") + int("25")
-
-#float() -> 轉為浮點數
-float("13.5") + float("21.7")
+#python使用的是動態型別
+x = 15
+print(type(x))
+x = 15.0
+print(type(x))
+x = '15.0'
+print(type(x))
+x = False
+print(type(x))
