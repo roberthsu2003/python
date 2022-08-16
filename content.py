da9400cd8442078f1cf6109f28b8db4025242c66
徐國堂 <roberthsu2003@gmail.com>
2022-08-16 20:13:26 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,13 +1,5 @@
-#請設計一個程式，讓使用者輸入數值，只有加總正偶數值，不加總正奇數值，如果輸入負數，結束程式。
-total = 0
-while True:
-  value = eval(input("請輸入數值:"))
-  if value < 0:
-    break
-  elif value % 2 == 1:
-    continue
-  else:
-    total += value
-
-print(f"total={total}")
-
+#python 純值
+'Hello! Python!'
+30
+30.0
+True
