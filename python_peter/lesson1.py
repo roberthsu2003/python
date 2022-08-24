405f166c5cbf2ecfd1ce803364cb153ffccb9a5c
徐國堂 <roberthsu2003@gmail.com>
2022-08-24 21:09:45 +0800

    modify

--- python_peter/lesson1.py
+++ python_peter/lesson1.py
@@ -8,8 +8,22 @@ def greatest_common_factor():
             print(f"最大公因數是:{i}")
             break
 
-def main():
+def least_common_multiple():
+    print("=========最小公倍數=============")
+    value1, value2 = eval(input("請輸入2數:(數值,數值)"))
+    max_value = max(value1, value2)
+    min_value = min(value1, value2)
+    i = 1
+    while True:
+        if max_value * i % min_value == 0:
+            LCM = max_value * i
+            break
+    i += 1
+    print(f"最小公倍數:{LCM}")
+
+def main():    
     greatest_common_factor()
+    least_common_multiple()
 
 if __name__ == "__main__":
     main()
