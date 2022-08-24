8f618e6d804d5ca96111a154afb879ed484975e3
徐國堂 <roberthsu2003@gmail.com>
2022-08-24 16:45:59 +0800

    modify

---
+++ 1108python應用基礎班/lesson8_covid19_country.py
@@ -0,0 +1,25 @@
+import requests
+import csv
+import pandas as pd
+
+url = 'https://od.cdc.gov.tw/eic/covid19/covid19_global_cases_and_deaths.csv'
+response = requests.get(url)
+if response.ok:
+    print("下載成功")
+
+response.encoding = "utf-8"
+csv_reader = csv.reader(response.text.splitlines())
+df = pd.DataFrame(csv_reader)
+df=df.iloc[1:]
+df.columns = ['國家','country', '確診', '死亡']
+df.set_index('國家',inplace=True)
+df['確診']=df['確診'].str.replace(",","")
+df['死亡']=df['死亡'].str.replace(",","")
+df['確診'] = df['確診'].astype(int)
+df['死亡'] = df['死亡'].astype(int)
+df["死亡比例"] = df["死亡"] / df["確診"] * 100
+for code in df.index:
+    print(code,end=',')
+print()
+countries = eval(input("請輸入國家:範例(xxx,xxxx,xxx):"))
+print(df.loc[countries])
