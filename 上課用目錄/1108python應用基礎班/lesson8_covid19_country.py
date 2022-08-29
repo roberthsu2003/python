28f072b402a06b93ff57090169df87f801c43fb8
徐國堂 <roberthsu2003@gmail.com>
2022-08-29 10:37:09 +0800

    modify

--- 上課用目錄/1108python應用基礎班/lesson8_covid19_country.py
+++ 上課用目錄/1108python應用基礎班/lesson8_covid19_country.py
@@ -1,24 +1,31 @@
 import requests
 import csv
 import pandas as pd
-def main():
+def loadData():
     url = 'https://od.cdc.gov.tw/eic/covid19/covid19_global_cases_and_deaths.csv'
     response = requests.get(url)
     if response.ok:
         print("下載成功")
 
     response.encoding = "utf-8"
-    csv_reader = csv.reader(response.text.splitlines())
+    return response.text.splitlines()
+
+def get_df(csv_text):
+    csv_reader = csv.reader(csv_text)
     df = pd.DataFrame(csv_reader)
-    df=df.iloc[1:]
-    df.columns = ['國家','country', '確診', '死亡']
-    df.set_index('國家',inplace=True)
-    df['確診']=df['確診'].str.replace(",","")
-    df['死亡']=df['死亡'].str.replace(",","")
+    df = df.iloc[1:]
+    df.columns = ['國家', 'country', '確診', '死亡']
+    df.set_index('國家', inplace=True)
+    df['確診'] = df['確診'].str.replace(",", "")
+    df['死亡'] = df['死亡'].str.replace(",", "")
     df['確診'] = df['確診'].astype(int)
     df['死亡'] = df['死亡'].astype(int)
     df["死亡比例"] = df["死亡"] / df["確診"] * 100
-    print(df)
+    return df
+
+def main():
+    all_text = loadData()
+    df = get_df(all_text)
     for code in df.index:
         print(code,end=',')
     print()
@@ -28,4 +35,5 @@ def main():
     with pd.ExcelWriter(f"{countries_str}_covid19.xlsx") as writer:
         country_df.to_excel(writer,sheet_name=countries_str)
 
-main()
+if __name__ == "__main__":
+    main()
