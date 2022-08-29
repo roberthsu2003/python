00f5a541e462e5ff49332ff8247a03b4850dcc57
徐國堂 <roberthsu2003@gmail.com>
2022-08-29 14:38:59 +0800

    modify

--- 上課用目錄/1108python應用基礎班/lesson10_2.py
+++ 上課用目錄/1108python應用基礎班/lesson10_2.py
@@ -3,11 +3,11 @@ import tkinter as tk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        tk.Frame(self,width=700,height=250,borderwidth=1,relief=tk.GROOVE).grid(column=0,row=0,padx=20,pady=20)
 
 def main():
     window = Window()
-    window.title("我的第一個視窗")
-    window.geometry("800x300")
+    window.title("全球_covid19_最新統計")
     window.mainloop()
 
 if __name__ == "__main__":
