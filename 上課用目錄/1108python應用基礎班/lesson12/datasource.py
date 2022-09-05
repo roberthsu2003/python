265b575078e941610eac065bf1e95925777d1ad4
徐國堂 <roberthsu2003@gmail.com>
2022-09-05 14:22:27 +0800

    modify

--- 上課用目錄/1108python應用基礎班/lesson12/datasource.py
+++ 上課用目錄/1108python應用基礎班/lesson12/datasource.py
@@ -2,3 +2,11 @@
 '''
 專門負責下載資料
 '''
+import requests
+
+def download_pm25():
+    url = "https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=json"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(response.text)
