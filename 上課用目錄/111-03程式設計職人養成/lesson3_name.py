bebba64cf45389822b080337c6dca77772313b0b
roberthsu2003 <roberthsu2003@gmail.com>
2022-09-06 16:32:11 +0800

    modify

---
+++ 上課用目錄/111-03程式設計職人養成/lesson3_name.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main():
+    window = tk.Tk()
+    window.title("這是我第一個window")
+    window.geometry("800x300")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
