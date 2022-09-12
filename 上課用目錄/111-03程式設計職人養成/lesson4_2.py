ba5734434217e4c96f09ff402b7b17d0e2022194
roberthsu2003 <roberthsu2003@gmail.com>
2022-09-12 10:37:52 +0800

    modify

--- 上課用目錄/111-03程式設計職人養成/lesson4_2.py
+++ 上課用目錄/111-03程式設計職人養成/lesson4_2.py
@@ -1,8 +1,13 @@
-class Window():
-    pass
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
 
 def main():
     window = Window()
+    window.mainloop()
 
 if __name__ == '__main__':
     main()
