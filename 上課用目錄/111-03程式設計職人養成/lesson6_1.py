40a1b6e1ef460c996054136cef9de8efa8000811
roberthsu2003 <roberthsu2003@gmail.com>
2022-09-12 15:56:13 +0800

    modify

--- 上課用目錄/111-03程式設計職人養成/lesson6_1.py
+++ 上課用目錄/111-03程式設計職人養成/lesson6_1.py
@@ -4,9 +4,15 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("上市公司每月營業收入彙種表")
-        tk.Label(self,text="上市公司每月營業收入彙種表",font=("arial",20)).pack()
+        tk.Label(self,text="上市公司每月營業收入彙種表",font=("arial",20)).pack(padx=50, pady=50)
+        tk.Button(self,text="下載資料",font=("arial",20),command=self.download).pack(padx=100,pady=50)
+    
+    def download(self):
+        print("download")
+
 
 def main():
+   url = "https://mopsfin.twse.com.tw/opendata/t187ap05_L.csv" 
    window = Window()
    window.mainloop()
 
