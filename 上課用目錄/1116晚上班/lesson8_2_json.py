877cb815332a9f423f35a0859f22d8041a6f05f7
徐國堂 <roberthsu2003@gmail.com>
2022-09-13 21:48:24 +0800

    modify

--- 上課用目錄/1116晚上班/lesson8_2_json.py
+++ 上課用目錄/1116晚上班/lesson8_2_json.py
@@ -3,33 +3,39 @@ import json
 from datetime import datetime
 import csv
 
-url = "http://api.openweathermap.org/data/2.5/group?id=1668341,1668399,1670481&units=metric&appid=29c4f184354b9889e87f7b494ac86aed"
-res = requests.get(url)
 
-if res.ok:
-    print("下載成功")
-    jsonText = res.text
-else:
-    print("下載失敗")
 
-data = json.loads(jsonText)
+def main():
+    url = "http://api.openweathermap.org/data/2.5/group?id=1668341,1668399,1670481&units=metric&appid=29c4f184354b9889e87f7b494ac86aed"
+    res = requests.get(url)
 
-datalist = data["list"]
-weathers = []
-for item in datalist:
-    city = [item['dt'],item['name'],item['coord']['lat'],item['coord']['lon'],item['main']['temp']]
-    weathers.append(city)
+    if res.ok:
+        print("下載成功")
+        jsonText = res.text
+    else:
+        print("下載失敗")
 
-for city in weathers:
-    timestamp = city[0]
-    dt_object = datetime.fromtimestamp(timestamp)
-    city[0] = dt_object.isoformat()
+    data = json.loads(jsonText)
 
-weathers.insert(0,['日期','城市','緯度','經度','攝氏溫度'])
+    datalist = data["list"]
+    weathers = []
+    for item in datalist:
+        city = [item['dt'],item['name'],item['coord']['lat'],item['coord']['lon'],item['main']['temp']]
+        weathers.append(city)
 
-with open("weathers.csv",mode="w",encoding="utf-8",newline='') as file:
-    csv_writer = csv.writer(file)
-    csv_writer.writerows(weathers)
-print("寫入成功")
+    for city in weathers:
+        timestamp = city[0]
+        dt_object = datetime.fromtimestamp(timestamp)
+        city[0] = dt_object.isoformat()
+
+    weathers.insert(0,['日期','城市','緯度','經度','攝氏溫度'])
+
+    with open("weathers.csv",mode="w",encoding="utf-8",newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerows(weathers)
+    print("寫入成功")
+
+if __name__ == "__main__":
+    main()
 
 
