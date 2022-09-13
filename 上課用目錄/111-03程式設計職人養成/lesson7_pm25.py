2c97b87be201213190e42bf42308e720c132d9b0
徐國堂 <roberthsu2003@gmail.com>
2022-09-13 14:38:27 +0800

    modify

--- 上課用目錄/111-03程式設計職人養成/lesson7_pm25.py
+++ 上課用目錄/111-03程式設計職人養成/lesson7_pm25.py
@@ -4,8 +4,9 @@ from io import StringIO
 import csv
 
 class Window(tk.Tk):
-    def __init__(self,county):
+    def __init__(self,csv_list,county):
         super().__init__()
+        self.csv_list =  csv_list
         self.county  = county
         self.title("空氣品質指標(AQI)概況")
         tk.Label(self,text="空氣品質指標(AQI)概況",font=("arial",20,"bold")).pack(padx=20,pady=50)
@@ -20,19 +21,24 @@ class Window(tk.Tk):
         print("button click")
         county_name = event.widget["text"]
         print(county_name)
+        
 
-def main():
-    county = get_county()
-    window = Window(county)    
+def main():    
+    csv_list = get_csvlist()
+    county = get_county(csv_list)
+    window = Window(csv_list,county)    
     window.mainloop()
 
-def get_county():
+def get_csvlist():
     url = "https://data.epa.gov.tw/api/v2/stat_p_115?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate desc&format=CSV"
     response = requests.get(url)
     response.encoding = "utf-8"
     with StringIO(response.text) as like_file:
         csv_reader = csv.reader(like_file)
         csv_list = list(csv_reader)
+    return csv_list
+
+def get_county(csv_list):    
     county_set = set()
     for item in csv_list:
         county_set.add(item[1])
