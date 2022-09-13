f9c0a37759cdd119caf796e4c09b348d6dc0c240
徐國堂 <roberthsu2003@gmail.com>
2022-09-13 11:31:05 +0800

    modify

---
+++ 上課用目錄/111-03程式設計職人養成/lesson7_pm25.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import requests
+from io import StringIO
+import csv
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+def main():
+    window = Window()
+    county = get_county()
+    print(county)
+    window.mainloop()
+
+def get_county():
+    url = "https://data.epa.gov.tw/api/v2/stat_p_115?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate desc&format=CSV"
+    response = requests.get(url)
+    response.encoding = "utf-8"
+    with StringIO(response.text) as like_file:
+        csv_reader = csv.reader(like_file)
+        csv_list = list(csv_reader)
+    county_set = set()
+    for item in csv_list:
+        county_set.add(item[1])
+    county_set.remove("item2")
+    county_set.remove("總計")
+    county = list(county_set)
+    return county
+
+if __name__ == "__main__":
+    main()
