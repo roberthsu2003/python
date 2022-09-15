3ff8c15e5acb0731c634275cd021527541098556
徐國堂 <roberthsu2003@gmail.com>
2022-09-15 21:55:03 +0800

    modify

--- 上課用目錄/1116晚上班/lesson10/index.py
+++ 上課用目錄/1116晚上班/lesson10/index.py
@@ -1,9 +1,21 @@
+from data import Person,Student
 import tkinter as tk
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("這是我的視窗程式")
+        tk.Button(self,text="請按我",command=self.userClick).pack(padx=100,pady=100)
+    
+    def userClick(self):
+        s1 = Student(n="jone",chinese=85)
+        s2 = Student(n="peter",age=21,english=92)
+        s1.description()
+        print("總分",s1.sum)
+        print("=============")
+        s2.description()
+        print("總分",s2.sum)
+
                
 
 def main():
