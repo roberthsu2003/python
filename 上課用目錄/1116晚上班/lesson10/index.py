a546399f5ac3b987832d901b573dfe1670f11fec
徐國堂 <roberthsu2003@gmail.com>
2022-09-15 21:44:46 +0800

    modify

--- 上課用目錄/1116晚上班/lesson10/index.py
+++ 上課用目錄/1116晚上班/lesson10/index.py
@@ -1,9 +1,14 @@
-class Window():
+import tkinter as tk
+
+class Window(tk.Tk):
     def __init__(self):
-        print("執行Window init")        
+        super().__init__()
+        self.title("這是我的視窗程式")
+               
 
 def main():
     window = Window()
+    window.mainloop()
 
 
 if __name__ == "__main__":
