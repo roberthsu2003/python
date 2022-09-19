20dd257587e7ff7f97e16d627c454c651351087b
徐國堂 <roberthsu2003@gmail.com>
2022-09-19 11:18:43 +0800

    modify

---
+++ 上課用目錄/111-03程式設計職人養成/lesson9/index.py
@@ -0,0 +1,26 @@
+def main():
+    tw_county_names = {"台北":"Taipei",
+                   "台中":"Taichung",
+                   "基隆":"Keelung",
+                   "台南":"Tainan",
+                   "高雄":"Kaohsiung",
+                   "新北":"New Taipei",
+                   "宜蘭":"Yilan",
+                   "桃園":"Taoyuan",
+                   "嘉義":"Chiayi",
+                   "新竹":"Hsinchu",
+                   "苗栗":"Miaoli",
+                   "南投":"Nantou",
+                   "彰化":"Changhua",
+                   "雲林":"Yunlin",
+                   "屏東":"Pingtung",
+                   "花蓮":"Hualien",
+                   "台東":"Taitung",
+                   "金門":"Kinmen",
+                   "澎湖":"Penghu",
+                   "連江":"Lienchiang"
+                   }
+    print(tw_county_names)
+
+if __name__ == "__main__":
+    main()
