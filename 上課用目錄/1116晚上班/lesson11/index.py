8fc6e2c43dec3e2efb55611377aa59095be6142a
徐國堂 <roberthsu2003@gmail.com>
2022-09-20 19:38:34 +0800

    modify

--- 上課用目錄/1116晚上班/lesson11/index.py
+++ 上課用目錄/1116晚上班/lesson11/index.py
@@ -3,7 +3,18 @@ import tkinter as tk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        
+        btn1=tk.Button(self,text="按鈕1",command=self.btn1_click,padx=30,pady=30)
+        btn1.pack(padx=100,pady=50)
+
+        btn2=tk.Button(self,text="按鈕2",command=self.btn2_click,padx=30,pady=30)
+        btn2.pack(padx=100,pady=50)
+
+    def btn1_click(self):
+        print("按鈕1的click")
+
+    def btn2_click(self):
+        print("按鈕2的click")
+
 
 def main():
     window=Window()
