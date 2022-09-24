a720e9150bbc38d3364ac15b9005973ed5974ae9
徐國堂 <roberthsu2003@gmail.com>
2022-09-24 10:20:18 +0800

    modify

--- 上課用目錄/python813/lesson11/main.py
+++ 上課用目錄/python813/lesson11/main.py
@@ -3,8 +3,9 @@ import tkinter as tk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("這是我的第一個視窗")
-        self.geometry("800x300")
+        self.title("這是我的第一個視窗")        
+        btn1 = tk.Button(self,text="下載資料",padx=50,pady=25,font=('Arial',20,'bold'))
+        btn1.pack(padx=100,pady=50)
 
 def main():
     #chinese和english區域變數
