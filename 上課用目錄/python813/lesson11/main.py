cc9bf1015c338ae0ad800a99df8233b6a1850635
徐國堂 <roberthsu2003@gmail.com>
2022-09-24 10:07:23 +0800

    modify

--- 上課用目錄/python813/lesson11/main.py
+++ 上課用目錄/python813/lesson11/main.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 
 class Window(tk.Tk):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.title("這是我的第一個視窗")
+        self.geometry("800x300")
 
 def main():
     #chinese和english區域變數
-    window = Window()
-    window.title("這是我的第一個視窗")
-    window.geometry("800x300")
+    window = Window()   
     window.mainloop()
 
 if __name__ == "__main__":
