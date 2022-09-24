aaf0b81ece1036c98c026bddf07c6b635afc3e0f
徐國堂 <roberthsu2003@gmail.com>
2022-09-24 14:44:17 +0800

    modify

--- 上課用目錄/python813/lesson11/datasource/__init__.py
+++ 上課用目錄/python813/lesson11/datasource/__init__.py
@@ -1,9 +1,15 @@
 import requests
 
-url = "https://data.epa.gov.tw/api/v2/aqx_p_133?limit=150&api_key=b8416fe0-3673-4eac-be88-7ac4bb9fce06"
+
 
 def download_air_data():
+    url = "https://data.epa.gov.tw/api/v2/aqx_p_133?limit=150&api_key=b8416fe0-3673-4eac-be88-7ac4bb9fce06"
     res = requests.request('GET',url)
     if res.ok:
         allData = res.json()
-    return allData['records']   
+    return allData['records']  
+
+def download_github_csv():
+    url = "https://raw.githubusercontent.com/roberthsu2003/python/master/%E4%B8%8A%E8%AA%B2%E7%94%A8%E7%9B%AE%E9%8C%84/python813/%E5%8F%B0%E5%8C%97%E5%B8%82youbike2.csv"
+    print("下載csv")
+    print(url)
