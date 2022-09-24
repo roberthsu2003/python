22d861055eac6d7e25889a1d87ac4eaf1d1987ce
徐國堂 <roberthsu2003@gmail.com>
2022-09-24 11:46:35 +0800

    modify

--- 上課用目錄/python813/lesson11/datasource/__init__.py
+++ 上課用目錄/python813/lesson11/datasource/__init__.py
@@ -3,4 +3,6 @@ import requests
 url = "https://data.epa.gov.tw/api/v2/aqx_p_133?limit=100&api_key=b8416fe0-3673-4eac-be88-7ac4bb9fce06"
 
 def download_air_data():
-    print("下載資料")
+    res = requests.request('GET',url)
+    if res.ok:
+        print("下載成功")
