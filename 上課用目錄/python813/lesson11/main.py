df5eb39f3adf9c95fba445d0ff9f086cb3ec0ce1
徐國堂 <roberthsu2003@gmail.com>
2022-09-24 09:27:20 +0800

    modify

--- 上課用目錄/python813/lesson11/main.py
+++ 上課用目錄/python813/lesson11/main.py
@@ -1,8 +1,8 @@
 
-chinese = 78
-english = 97
-
 if __name__ == "__main__":
+    chinese = 78
+    english = 97
     print("主執行檔")
-    print('chinese=',chinese,sep="")
-    print('english=',english,sep="")
+
+print('chinese=',chinese,sep="")
+print('english=',english,sep="")
