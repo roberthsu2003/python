aa0e5f92695d7efe6dfed83702be923b42b5ea91
徐國堂 <roberthsu2003@gmail.com>
2022-09-24 14:52:22 +0800

    modify

--- 上課用目錄/python813/lesson11/datasource/__init__.py
+++ 上課用目錄/python813/lesson11/datasource/__init__.py
@@ -12,4 +12,7 @@ def download_air_data():
 def download_github_csv():
     url = "https://raw.githubusercontent.com/roberthsu2003/python/master/%E4%B8%8A%E8%AA%B2%E7%94%A8%E7%9B%AE%E9%8C%84/python813/%E5%8F%B0%E5%8C%97%E5%B8%82youbike2.csv"
     print("下載csv")
-    print(url)
+    res = requests.request('GET',url)
+    if res.ok:
+        csvText = res.text
+    print(csvText)
