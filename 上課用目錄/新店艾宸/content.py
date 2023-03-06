6c1644f7b1fc219a527c90c5a1ac2b777e0208a3
徐國堂 <roberthsu2003@gmail.com>
2023-03-06 19:37:32 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,9 +1,11 @@
-#小明想要存錢買一輛機車,機車每輛30000元，他將每月存的錢輸入，當存款足夠買機車時，就顯示提示訊息告知。
+#請輸入小寫英文字[按q會離開]:taipei
+#Taipei
+#請輸入小寫英文字[按q會離開]:q
+#程式結束
 
-deposit = 0
-num = 0
-while(deposit<30000):
-  num += 1
-  inputNum = int(input(f"請輸入第{num}個月份的存款:"))
-  deposit += inputNum
-print(f"恭喜!已經存夠了,存了{num}個月,總存款為:{deposit}")
+stuff = input("請輸入小寫英文字[按q會離開]:")
+while(stuff != "q"):
+  print(stuff.capitalize())
+  stuff = input("請輸入小寫英文字[按q會離開]:")
+
+print("程式結束")
