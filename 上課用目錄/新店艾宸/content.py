70a3e9d753f16ae993bb27795edd986a07d78011
徐國堂 <roberthsu2003@gmail.com>
2023-03-06 19:24:01 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,2 +1,9 @@
-for (index, value) in enumerate('abcdefg'):
-  print(f"index={index},value={value}")
+#小明想要存錢買一輛機車,機車每輛30000元，他將每月存的錢輸入，當存款足夠買機車時，就顯示提示訊息告知。
+
+deposit = 0
+num = 0
+while(deposit<30000):
+  num += 1
+  inputNum = int(input(f"請輸入第{num}個月份的存款:"))
+  deposit += inputNum
+print(f"恭喜!已經存夠了,存了{num}個月,總存款為:{deposit}")
