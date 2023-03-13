d61729bd071f561ea38ed9493a9d4ad04c0c681a
徐國堂 <roberthsu2003@gmail.com>
2023-03-13 19:05:31 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -2,18 +2,30 @@ import random
 
 min = 1
 max = 100
+count = 0
 target = random.randint(min, max)
 
 print("============猜數字遊戲==============")
 
 while(True):
   keyin = int(input(f"猜數字範圍{min}~{max}:"))
-  if(keyin >= min and keyin <= max):
-    print("在範圍內")
+  count += 1
+  if(min <= keyin <= max):
+    if(keyin == target):
+      print(f"賓果!猜對了, 答案是:{target}")
+      print(f"您猜了{count}次")
+      break
+    elif(keyin > target):
+      print("再小一點")
+      max = keyin - 1
+    elif(keyin < target):
+      print("再大一點")
+      min = keyin + 1
+    print(f"您已猜了{count}次")
   else:
     print("請輸入提示範圍內的數字")
     continue
   
-  break
+  
 
 print("程式結束")
