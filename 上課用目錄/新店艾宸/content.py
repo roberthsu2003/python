96e1fdb7fec262b0929c250ef5a6f5fc26d029f6
徐國堂 <roberthsu2003@gmail.com>
2023-03-13 18:46:23 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,13 +1,6 @@
-#請設計一個程式，讓使用者輸入數值，只有加總正偶數值，不加總正奇數值，如果輸入負數，結束程式。
+import random
 
-sum = 0
-while(True):
-  inputNum = eval(input("請輸入數值:"))
-  if(inputNum < 0):
-    break
-  elif(inputNum % 2 == 1):
-    continue
-  else:
-    sum += inputNum
-
-print(f"所有輸入的正偶數的加總是:{sum}")
+min = 1
+max = 100
+target = random.randint(min, max)
+target
