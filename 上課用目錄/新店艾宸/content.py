4066971c348b23d6e3639b86a946e265cae7e34d
徐國堂 <roberthsu2003@gmail.com>
2023-03-13 19:41:28 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,47 +1,26 @@
-#定義了play_game的function
-def play_game():
-  import random
+#沒有參數,沒有傳出值的function
+def say_hello() -> None:
+  print("Hello!")
 
-  min = 1
-  max = 100
-  count = 0
-  target = random.randint(min, max)
 
-  print("============猜數字遊戲==============")
-  print(target)
-  while(True):
-    keyin = int(input(f"猜數字範圍{min}~{max}:"))
-    count += 1
-    if(min <= keyin <= max):
-      if(keyin == target):
-        print(f"賓果!猜對了, 答案是:{target}")
-        print(f"您猜了{count}次")
-        break
-      elif(keyin > target):
-        print("再小一點")
-        max = keyin - 1
-      elif(keyin < target):
-        print("再大一點")
-        min = keyin + 1
-      print(f"您已猜了{count}次")
-    else:
-      print("請輸入提示範圍內的數字")
-      continue
-    
-    
+say_hello()
 
-  print("程式結束")
-  
-  
-  
-  
-  
-  
-  
-  
-  
- while(True):
-  play_game()
-  play_again = input("您還要繼續嗎?(y,n)")
-  if(play_again == 'n'):
-    break
+
+
+
+
+#1個參數,沒有傳出值的function
+def say_greeting(name) -> None:
+  print(f"{name}您好")
+
+
+say_greeting("robert")
+
+
+
+#2個參數,一個傳出的值
+def add(a,b) -> float:
+  return a + b
+
+value = add(5,7)
+print(value)
