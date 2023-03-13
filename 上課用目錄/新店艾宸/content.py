5030b0cb91234ca40a1e8c947d1aed5ebc57aabf
徐國堂 <roberthsu2003@gmail.com>
2023-03-13 19:58:39 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,26 +1,16 @@
-#沒有參數,沒有傳出值的function
-def say_hello() -> None:
-  print("Hello!")
-
-
-say_hello()
+list1 = [1, 2, 3, 4, 5]
+list1[0] = 10
+print(list1[0])
 
 
 
 
 
-#1個參數,沒有傳出值的function
-def say_greeting(name) -> None:
-  print(f"{name}您好")
-
-
-say_greeting("robert")
-
-
-
-#2個參數,一個傳出的值
-def add(a,b) -> float:
-  return a + b
-
-value = add(5,7)
-print(value)
+empty = list()
+empty.append(10)
+empty.append(20)
+empty.append(30)
+empty.append(40)
+empty.append(50)
+empty.clear()
+empty
