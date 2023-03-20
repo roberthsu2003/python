e229b53decbb9f20e95f1c257b4ac02abd761b36
徐國堂 <roberthsu2003@gmail.com>
2023-03-20 19:48:48 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,10 +1,26 @@
-for key in dic1:
-  print(key)
-  print(dic1[key])
-  print("=======")
+import random
+def generate_scores() -> dict:
+  scores = {}
+  scores['chinese'] = random.randint(50,100)
+  scores['english'] = random.randint(50,100)
+  scores['math'] = random.randint(50,100)
+  scores['history'] = random.randint(50,100)
+  scores['discover'] = random.randint(50,100)
+  return scores
+
+generate_scores()
 
 
-for key,value in dic1.items():
-  print(key)
-  print(value)
-  print("=======")
+
+
+students = []
+for _ in range(50):
+  students.append(generate_scores())
+
+for student in students:
+  print(f"國文:{student['chinese']}")
+  print(f"英文:{student['english']}")
+  print(f"數學:{student['math']}")
+  print(f"歷史:{student['history']}")
+  print(f"創造:{student['discover']}")
+  print("==========================")
