add8d546affe36e27f6071035217642871bd08d0
徐國堂 <roberthsu2003@gmail.com>
2023-03-20 19:01:49 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,7 +1,22 @@
-for letter in 'abcdefg':
-  print(letter)
-  
-  
-  
- for score in (56, 92, 35, 73, 93):
+ls1 = [78, 87, 68, 92, 91]
+ls1
+
+
+ls2 = list('letter')
+ls2
+
+
+for score in ls1:
   print(score)
+  
+  
+ls1[0]
+ls1[0] = 100
+ls1[0]
+
+
+
+ls1[0], ls1[2]
+
+
+ls1[5]
