5a454092491031b7b708af76096cf6744245933c
徐國堂 <roberthsu2003@gmail.com>
2023-03-20 19:23:44 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,15 +1,13 @@
-stu1 = []
-stu1.append(78)
-stu1.append(69)
-stu1.append(95)
-stu1.append(79)
-stu1.append(61)
-stu1
+import random
 
+def generate_scores() -> list:
+  scores = []
+  for i in range(5):
+    scores.append(random.randint(50, 100))
+  return scores
 
+students = []
+for i in range(50):
+  students.append(generate_scores())
 
-stu1.pop()
-stu1
-
-
-student_3[0][4], student_3[1][2], student_3[2][4]
+students
