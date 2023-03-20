5c93ebbb652b562902af31878ea2956d405fb9e1
徐國堂 <roberthsu2003@gmail.com>
2023-03-20 18:47:27 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,16 +1,27 @@
-list1 = [1, 2, 3, 4, 5]
-list1[0] = 10
-print(list1[0])
+x = 10
+y = 15.3
+print(x)
+print(y)
 
 
 
+point = (10, 15.3)
+point
 
 
-empty = list()
-empty.append(10)
-empty.append(20)
-empty.append(30)
-empty.append(40)
-empty.append(50)
-empty.clear()
-empty
+point = 10, 15.3
+point
+
+
+x, y = point
+print(x)
+print(y)
+
+
+def generate_point() -> tuple:
+  return 10, 15.3
+
+x, y = generate_point()
+
+x, y
+
