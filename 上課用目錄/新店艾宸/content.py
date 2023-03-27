b7fb9bfdd58b17d5172a43f7d166564daf6377bd
徐國堂 <roberthsu2003@gmail.com>
2023-03-27 19:43:25 +0800

    Update content.py

--- 上課用目錄/新店艾宸/content.py
+++ 上課用目錄/新店艾宸/content.py
@@ -1,4 +1,8 @@
+def printPicnic(itemDict, leftWidth, rightWidth):
+  print("PICNIC ITEMS".center(leftWidth+rightWidth,'-'))
+  for k,v in itemDict.items():
+    print(k.ljust(leftWidth,'.') + str(v).rjust(rightWidth))
+
 picnicItems = {"sandwiches":4, 'apple':12, 'cups':4, 'cookies':34}
-print("PICNIC ITEMS".center(17,'-'))
-for k,v in picnicItems.items():
-  print(k.ljust(12,'.') + str(v).rjust(5))
+printPicnic(picnicItems,12,5)
+printPicnic(picnicItems,20,6)
