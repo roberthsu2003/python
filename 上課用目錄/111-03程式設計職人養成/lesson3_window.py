005a041a4e9060baa408d6053c4ceca6b7f41dfc
徐國堂 <roberthsu2003@gmail.com>
2023-03-29 18:42:50 +0800

    modify

--- 上課用目錄/111-03程式設計職人養成/lesson3_window.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-if __name__ ==  "__main__":
-    window = tk.Tk()
-    window.title("這是我第一個window")
-    window.geometry("800x300")
-    window.mainloop()
--- 上課用目錄/1116晚上班/lesson9_4.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("main主程式執行了")
-
-if __name__ == "__main__":
-    main()
