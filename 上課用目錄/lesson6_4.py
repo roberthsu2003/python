85c5c7c29000a25fe3aea006e0a7cac0bb5c77f4
徐國堂 <roberthsu2003@gmail.com>
2023-09-11 13:35:02 +0000

    modify

--- 上課用目錄/lesson6_4.py
+++ 上課用目錄/lesson6_4.py
@@ -34,5 +34,9 @@ def play_game():
                 print("輸入錯誤,超出範圍")
                 print(f"您已經猜了{count}次")
 
-play_game()
+while(True):
+    play_game()
+    play_again = input("請問還要繼續嗎?y,n:")
+    if play_again == 'n':
+        break
 print("遊戲結束")
